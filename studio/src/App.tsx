9960d461125b9a406681939f01d9b29b4b554222
Enes Malik Ozer <enes@altogic.com>
2023-06-06 00:11:52 +0300

    feat: initialize i18next with english resources

--- studio/src/App.tsx
+++ studio/src/App.tsx
@@ -1,8 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { useTranslation } from "react-i18next";
 
 function App() {
-	return <RouterProvider router={router} />;
+	const { t } = useTranslation();
+	return (
+		<>
+			<RouterProvider router={router}></RouterProvider>
+			<h1>{t("title")}</h1>
+			<h4>{t("description")}</h4>
+		</>
+	);
 }
 
 export default App;
