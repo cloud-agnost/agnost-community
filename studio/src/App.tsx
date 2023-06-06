c30c005de0c59b2ba7e5a5eef00879f79f1fe7ff
Özgür ÖZALP <ozgur@altogic.com>
2023-06-07 00:20:48 +0300

    slider added

--- studio/src/App.tsx
+++ studio/src/App.tsx
@@ -1,18 +1,8 @@
-import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-import { useTranslation } from 'react-i18next';
 
 function App() {
-	const { t } = useTranslation();
-	return (
-		<div className='bg-base'>
-			<RouterProvider router={router}></RouterProvider>
-			<h1 className='text-default text-3xl'>{t('title')}</h1>
-			<h4 className='text-elements-strong-red'>{t('description')}</h4>
-			<h4 className='text-surface-orange'>{t('description')}</h4>
-		</div>
-	);
+	return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
