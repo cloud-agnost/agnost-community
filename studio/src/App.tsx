de505a5b49813d131575a82aa503486afcd32202
Enes Malik Ozer <enes@altogic.com>
2023-06-06 16:01:10 +0300

    tailiwnd and prettier configs setup

--- studio/src/App.tsx
+++ studio/src/App.tsx
@@ -1,15 +1,17 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
-import { useTranslation } from "react-i18next";
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+import { useTranslation } from 'react-i18next';
 
 function App() {
 	const { t } = useTranslation();
 	return (
-		<>
+		<div className='bg-base'>
 			<RouterProvider router={router}></RouterProvider>
-			<h1>{t("title")}</h1>
-			<h4>{t("description")}</h4>
-		</>
+			<h1 className='text-default text-3xl'>{t('title')}</h1>
+			<h4 className='text-elements-strong-red'>{t('description')}</h4>
+			<h4 className='text-surface-orange'>{t('description')}</h4>
+		</div>
 	);
 }
 
