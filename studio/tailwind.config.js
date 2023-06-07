8cb1830b01200dd527194ca9d6763b557c36d5ec
Enes Malik Ozer <enes@altogic.com>
2023-06-07 16:56:04 +0300

    feat(tailwind.config.js): add support for [data-mode="dark"] attribute in darkMode option

--- studio/tailwind.config.js
+++ studio/tailwind.config.js
@@ -1,7 +1,7 @@
 import { fontFamily } from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: ['class'],
+	darkMode: ['class', '[data-mode="dark"]'],
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		container: {
