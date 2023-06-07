e92c051af42f4c830a5e7e408b76ccd2e5a8e3e0
Özgür ÖZALP <ozgur@altogic.com>
2023-06-07 15:38:55 +0300

    Revert "Merge pull request #3 from cloud-agnost/slider"
    This reverts commit 55f722493e7a93e72c2f6541226d3e754bd85e77, reversing
    changes made to db64900e1ff62b2708b3e9826fc22d0584c152a7.

--- studio/src/router/index.tsx
+++ studio/src/router/index.tsx
@@ -1,29 +1,9 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Slider } from '@/components/Slider';
-
-const items = [
-	{
-		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto assumenda explicabo ipsa modi nesciunt nisi, nobis, provident quaerat qui quis, saepe sed sint soluta sunt voluptatem voluptatibus! Animi, officiis?',
-		element: <img src='https://shaders-slider.uiinitiative.com/images/01.jpg' alt='' />,
-	},
-	{
-		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto assumenda explicabo ipsa modi nesciunt nisi, nobis, provident quaerat qui quis, saepe sed sint soluta sunt voluptatem voluptatibus! Animi, officiis?',
-		element: <img src='https://shaders-slider.uiinitiative.com/images/02.jpg' alt='' />,
-	},
-];
+import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
 	{
-		path: '/',
-		element: <div className=' font-bold text-element-strong-blue text-3xl'>Hello Agnost</div>,
-	},
-	{
-		path: '/slider',
-		element: (
-			<div className='flex p-10 items-center justify-center'>
-				<Slider items={items} />
-			</div>
-		),
+		path: "/",
+		element: <div>Hello Agnost</div>,
 	},
 ]);
 
