457d63558d3f2799e5297fe225387ea6800ffc3b
Özgür ÖZALP <ozgur@altogic.com>
2023-06-14 00:01:46 +0300

    fix: Redirect to Login Page for Cluster Setup Completion and No Active Session

--- studio/src/pages/root/Root.tsx
+++ studio/src/pages/root/Root.tsx
@@ -1,14 +1,21 @@
-import { Outlet, redirect } from 'react-router-dom';
+import { LoaderFunctionArgs, Outlet, redirect } from 'react-router-dom';
 import useClusterStore from '@/store/cluster/clusterStore.ts';
+import useAuthStore from '@/store/auth/authStore.ts';
+import { removeLastSlash } from 'utils/utils.ts';
 
-async function loader() {
+async function loader(params: LoaderFunctionArgs) {
 	const status = await useClusterStore.getState().checkClusterSetup();
 	await useClusterStore.getState().checkClusterSmtpStatus();
+	const isAuthenticated = useAuthStore.getState().isAuthenticated();
 
-	// TODO: Check if user is logged in
+	const requestURL = new URL(params.request.url);
+
 	if (!status) {
 		return redirect('/onboarding');
+	} else if (status && !isAuthenticated && removeLastSlash(requestURL.pathname) !== '/login') {
+		return redirect('/login');
 	}
+
 	return null;
 }
 
