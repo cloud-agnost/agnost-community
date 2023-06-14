3284ec4d8eb807f13386848709f4beeda244c71b
Enes Malik Ozer <enes@altogic.com>
2023-06-14 20:08:41 +0300

    feat(Slider.tsx): add new props to Slider component
    - Add loop prop to enable/disable infinite loop of slides
    - Add autoplay prop to enable/disable autoplay of slides
    - Add spaceBetween prop to set the space between slides
    - Add pagination prop to enable/disable pagination
    - Add slidesPerView prop to set the number of slides per view
    - Add grid prop to enable/disable grid mode
    - Add freeMode prop to enable/disable free mode
    - Add support for ReactElement as an item element in the items prop

--- studio/src/components/Slider/Slider.tsx
+++ studio/src/components/Slider/Slider.tsx
@@ -1,34 +1,63 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
 import { cn } from '@/utils';
+import { ReactElement } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { Autoplay, Pagination } from 'swiper';
+import { Autoplay, FreeMode, Grid, Pagination } from 'swiper';
+import { GridOptions } from 'swiper/types';
 import './Slider.scss';
 
 interface SliderProps {
 	className?: string;
+	loop?: boolean;
+	autoplay?: boolean;
+	spaceBetween?: number;
+	pagination?: boolean;
+	slidesPerView?: number;
+	grid?: GridOptions;
+	freeMode?: boolean;
+
 	items: {
-		text: string;
-		image: string;
+		text?: string;
+		element: ReactElement;
 	}[];
 }
 
-export default function Slider({ items, className }: SliderProps) {
+export default function Slider({
+	items,
+	className,
+	loop,
+	autoplay,
+	slidesPerView = 1,
+	spaceBetween = 10,
+	pagination,
+	grid,
+	freeMode,
+
+	...props
+}: SliderProps) {
 	return (
 		<Swiper
-			spaceBetween={10}
-			pagination
-			loop
-			autoplay
+			spaceBetween={spaceBetween}
+			pagination={pagination}
+			freeMode={freeMode}
+			loop={loop}
+			autoplay={autoplay}
 			className={cn('slider', className)}
-			modules={[Pagination, Autoplay]}
+			slidesPerView={slidesPerView}
+			grid={grid}
+			modules={[
+				pagination && Pagination,
+				autoplay && Autoplay,
+				!!grid && Grid,
+				freeMode && FreeMode,
+			].filter(Boolean)}
+			{...props}
 		>
-			{items.map(({ image, text }, index) => (
+			{items.map(({ element, text }, index) => (
 				<SwiperSlide className='slider-item' key={index}>
-					<div className='slider-item-cover'>
-						<img src={image} alt={text} />
-					</div>
-					<div className='slider-item-text'>{text}</div>
+					<div className='slider-item-cover'>{element}</div>
+					{text && <div className='slider-item-text'>{text}</div>}
 				</SwiperSlide>
 			))}
 		</Swiper>
