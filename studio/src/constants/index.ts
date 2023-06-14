924bd2fb341a391a48a2834fba6c796a0a391271
Enes Malik Ozer <enes@altogic.com>
2023-06-14 14:07:39 +0300

    feat(constants, AuthLayout): add slider images to constants and use them in AuthLayout

---
+++ studio/src/constants/index.ts
@@ -0,0 +1,18 @@
+import Database from '@/assets/images/database.png';
+import Rapid from '@/assets/images/rapid.png';
+import Realtime from '@/assets/images/realtime.png';
+
+export const SLIDER_IMAGES = [
+	{
+		text: 'Accelerate your app development journey and leave the competition in the dust with our cutting-edge platform designed for rapid innovation and unbeatable efficiency.',
+		image: Rapid,
+	},
+	{
+		text: 'Amplify your backend capabilities and seamlessly integrate, manipulate, and leverage the power of multiple databases, including MySQL, MSSQL, and PostgreSQL.',
+		image: Database,
+	},
+	{
+		text: 'Amplify your backend capabilities and seamlessly integrate, manipulate, and leverage the power of multiple databases, including MySQL, MSSQL, and PostgreSQL.',
+		image: Realtime,
+	},
+];
