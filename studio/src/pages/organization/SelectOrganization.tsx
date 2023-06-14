fb271268cc92cbdd0207616bd0dcb5909c01c037
Enes Malik Ozer <enes@altogic.com>
2023-06-14 20:10:54 +0300

    feat(SelectOrganization.tsx): add slider component to display organizations feat(SelectOrganization.tsx): add OrganizationCreateButton component to create new organization feat(SelectOrganization.tsx): add OrganizationCreateModal component to create new organization feat(organization.scss): add styles for SelectOrganization component

--- studio/src/pages/organization/SelectOrganization.tsx
+++ studio/src/pages/organization/SelectOrganization.tsx
@@ -1,6 +1,12 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/Avatar';
+import { Slider } from '@/components/Slider';
+import { OrganizationCreateButton, OrganizationCreateModal } from '@/features/Organization';
 import useOrganizationStore from '@/store/organization/organizationStore';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import useAuthStore from '@/store/auth/authStore';
+import './organization.scss';
+import { useTranslation } from 'react-i18next';
 
 async function loader() {
 	return useOrganizationStore.getState().getAllOrganizationByUser();
@@ -8,30 +14,48 @@ async function loader() {
 
 export default function SelectOrganization() {
 	const { organizations } = useOrganizationStore();
+	const [isOpen, setIsOpen] = useState(false);
+	const { user } = useAuthStore();
+	const { t } = useTranslation();
 	return (
 		<div>
 			{organizations.length > 0 && (
-				<div className='my-10 md:my-0'>
-					<div className='flex flex-col items-center justify-center gap-8 lg:gap-20 md:h-screen'>
-						<h1 className='text-default text-xl md:text-3xl font-semibold tracking-md'>
-							Select Your Organization
-						</h1>
-						<div className='max-w-sm sm:max-w-lg lg:max-w-xl'>
-							<div className='flex flex-wrap justify-center gap-x-16 gap-y-8 md:gap-16 lg:gap-24'>
-								{organizations.map((organization) => (
-									<Link to={`/organization/${organization?._id}`} key={organization?._id}>
-										<Avatar size='xl'>
-											<AvatarImage />
-											<AvatarFallback name={organization?.name} color={organization?.color} />
-										</Avatar>
-									</Link>
-								))}
-								{/* {user?.canCreateCompany && <CompanyButton label='Create company' isCreateCompany />} */}
-							</div>
+				<>
+					<div className='select-organization-container'>
+						<h1 className='select-organization-title'>{t('organization.select')}</h1>
+
+						<div className='select-organization-slider'>
+							<Slider
+								slidesPerView={4}
+								spaceBetween={32}
+								freeMode
+								items={organizations.map((organization) => {
+									return {
+										element: (
+											<Link to={`/organization/${organization?._id}`} key={organization?._id}>
+												<div className='select-organization-item'>
+													<Avatar size='xl'>
+														<AvatarImage src={organization.pictureUrl} alt={organization.name} />
+														<AvatarFallback name={organization?.name} color={organization?.color} />
+													</Avatar>
+													<div className='select-organization-info'>
+														<p className='select-organization-name'>{organization?.name}</p>
+														<p className='select-organization-role'>{organization?.role}</p>
+													</div>
+												</div>
+											</Link>
+										),
+									};
+								})}
+							/>
+							{user?.canCreateOrg && (
+								<OrganizationCreateButton onClick={() => setIsOpen(!isOpen)} />
+							)}
 						</div>
 					</div>
-				</div>
+				</>
 			)}
+			<OrganizationCreateModal isOpen={isOpen} closeModal={() => setIsOpen(!isOpen)} />
 		</div>
 	);
 }
