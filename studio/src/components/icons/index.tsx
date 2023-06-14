18c8a46c0ac6a7548e75183fc69db923d3c71a22
Özgür ÖZALP <ozgur@altogic.com>
2023-06-14 20:39:26 +0300

    add: icons for settings page

--- studio/src/components/icons/index.tsx
+++ studio/src/components/icons/index.tsx
@@ -1,6 +1,8 @@
 export { default as AgnostOnlyLogo } from './AgnostOnlyLogo';
+export { default as BellRing } from './BellRing';
 export { default as ChangeLog } from './ChangeLog';
 export { default as Error } from './Error';
 export { default as LightBulb } from './LightBulb';
+export { default as LineSegments } from './LineSegments';
 export { default as SuccessCheck } from './SuccessCheck';
 export { default as Warning } from './Warning';
