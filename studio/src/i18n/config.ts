622bbc95eacacc0148df66403f098fa30e94425c
Özgür ÖZALP <ozgur@altogic.com>
2023-06-15 00:57:17 +0300

    add: profile settings languages

--- studio/src/i18n/config.ts
+++ studio/src/i18n/config.ts
@@ -1,6 +1,6 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { login, forms, general, organization } from './en';
+import { forms, general, login, organization } from './en';
 
 export const resources = {
 	en: {
@@ -13,9 +13,16 @@ export const resources = {
 	},
 };
 
-i18next.use(initReactI18next).init({
-	lng: 'en', // if you're using a language detector, do not define the lng option
-	debug: true,
-	resources,
-	defaultNS: 'translation',
-});
+i18next
+	.use(initReactI18next)
+	.init({
+		lng: 'en', // if you're using a language detector, do not define the lng option
+		debug: true,
+		resources,
+		defaultNS: 'translation',
+	})
+	.catch(console.error);
+
+export const { t } = i18next;
+
+export default i18next;
