de6f689fd018b1f6229e70ddbadf80cc9591cd73
Enes Malik Ozer <enes@altogic.com>
2023-06-16 12:09:30 +0300

    feat(App.tsx): add ToastContainer component to display notifications

--- studio/src/App.tsx
+++ studio/src/App.tsx
@@ -1,11 +1,29 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
 import { useRenewToken } from '@/hooks';
 import '@/i18n/config';
+import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import router from './router';
 
 function App() {
 	useRenewToken(2);
-	return <RouterProvider router={router} />;
+	return (
+		<>
+			<RouterProvider router={router} />
+			<ToastContainer
+				position='top-right'
+				autoClose={5000}
+				hideProgressBar
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme='light'
+			/>
+		</>
+	);
 }
 
 export default App;
