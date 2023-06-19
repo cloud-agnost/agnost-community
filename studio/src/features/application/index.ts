1e7c64ee6ef4d6047c523e39d89ea32f8d8c3768
Enes Malik Ozer <enes@altogic.com>
2023-06-19 20:20:41 +0300

    feat(ApplicationTable): add ApplicationTable component feat(ApplicationColumns): add columns for ApplicationTable feat(ApplicationTeam): add ApplicationTeam component to export

--- studio/src/features/application/index.ts
+++ studio/src/features/application/index.ts
@@ -1,2 +1,3 @@
 export { default as ApplicationCard } from './ApplicationCard';
 export { default as ApplicationSettings } from './ApplicationSettings';
+export { default as ApplicationTeam } from './ApplicationTeam';
