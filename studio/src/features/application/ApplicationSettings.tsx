ba6d4518e9037b7bb988612d111b681442f2c2af
Enes Malik Ozer <enes@altogic.com>
2023-06-19 12:54:25 +0300

    refactor(ApplicationSettings.tsx): replace Button with Command and CommandItem components feat(ApplicationSettings.tsx): add support for leave team and delete options in the popover menu

--- studio/src/features/application/ApplicationSettings.tsx
+++ studio/src/features/application/ApplicationSettings.tsx
@@ -1,33 +1,59 @@
-import { DotsThreeVertical } from '@phosphor-icons/react';
+import { Button } from '@/components/Button';
+import { Command, CommandGroup, CommandItem, CommandSeparator } from '@/components/Command';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/Popover';
 import { APPLICATION_SETTINGS } from '@/constants';
-import { Button } from '@/components/Button';
+import { DotsThreeVertical } from '@phosphor-icons/react';
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 export default function ApplicationSettings() {
+	const { t } = useTranslation();
 	const [open, setOpen] = useState(false);
 	return (
 		<Popover open={open} onOpenChange={setOpen}>
 			<PopoverTrigger asChild>
-				<Button variant='blank' iconOnly className='ml-auto'>
+				<Button variant='blank' iconOnly className='p-0'>
 					<DotsThreeVertical className='w-5 h-5 text-icon-secondary' />
 					<span className='sr-only'>Open popover</span>
 				</Button>
 			</PopoverTrigger>
-			<PopoverContent className='w-80'>
-				<div className=''>
-					{APPLICATION_SETTINGS.map((setting) => (
-						<Button
-							variant='blank'
-							key={setting.name}
-							onClick={() => {
+			<PopoverContent className='w-[181px]'>
+				<Command>
+					<CommandGroup>
+						{APPLICATION_SETTINGS.map((setting) => (
+							<CommandItem
+								key={setting.name}
+								onSelect={() => {
+									setOpen(false);
+								}}
+								className='font-sfCompact px-3'
+							>
+								{setting.name}
+							</CommandItem>
+						))}
+					</CommandGroup>
+					<CommandSeparator />
+					<CommandGroup>
+						<CommandItem
+							onSelect={() => {
 								setOpen(false);
 							}}
-							className='w-full justify-start'
+							className='font-sfCompact px-3'
 						>
-							<span className='text-sm text-default'>{setting.name}</span>
-						</Button>
-					))}
-				</div>
+							{t('application.settings.leaveTeam')}
+						</CommandItem>
+					</CommandGroup>
+					<CommandSeparator />
+					<CommandGroup>
+						<CommandItem
+							onSelect={() => {
+								setOpen(false);
+							}}
+							className='font-sfCompact px-3'
+						>
+							{t('general.delete')}
+						</CommandItem>
+					</CommandGroup>
+				</Command>
 			</PopoverContent>
 		</Popover>
 	);
