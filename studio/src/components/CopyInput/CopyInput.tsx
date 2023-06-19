49a3db470bdbf5dd134007f73a7df62649fb17ab
Özgür ÖZALP <ozgur@altogic.com>
2023-06-19 11:45:44 +0300

    add: copy input status as a toast

--- studio/src/components/CopyInput/CopyInput.tsx
+++ studio/src/components/CopyInput/CopyInput.tsx
@@ -5,10 +5,31 @@ import * as React from 'react';
 import { useState } from 'react';
 import { Button } from '../Button';
 import './copyInput.scss';
+import { useToast } from '@/hooks';
+import { useTranslation } from 'react-i18next';
 
 const CopyInput = React.forwardRef<HTMLInputElement, React.ComponentPropsWithoutRef<'input'>>(
 	({ className, value, readOnly, placeholder, ...props }, ref) => {
 		const [inputValue, setInputValue] = useState<string>(value as string);
+		const { notify } = useToast();
+		const { t } = useTranslation();
+
+		async function copy() {
+			try {
+				await navigator.clipboard.writeText(inputValue);
+				notify({
+					title: t('general.success'),
+					description: t('general.copied'),
+					type: 'success',
+				});
+			} catch (e) {
+				notify({
+					title: t('general.error'),
+					description: t('general.copied_error'),
+					type: 'error',
+				});
+			}
+		}
 
 		return (
 			<div className={cn('copy-input-wrapper', className)} {...props}>
@@ -20,12 +41,7 @@ const CopyInput = React.forwardRef<HTMLInputElement, React.ComponentPropsWithout
 					placeholder={placeholder}
 					className='copy-input'
 				/>
-				<Button
-					className='copy-input-button'
-					onClick={() => navigator.clipboard.writeText(inputValue)}
-					variant='blank'
-					type='button'
-				>
+				<Button className='copy-input-button' onClick={copy} variant='blank' type='button'>
 					<Copy size={20} />
 				</Button>
 			</div>
