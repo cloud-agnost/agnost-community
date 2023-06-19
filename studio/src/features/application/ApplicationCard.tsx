ffbe451a222ec3c8b269e1b1d20a332624414048
Enes Malik Ozer <enes@altogic.com>
2023-06-19 12:54:12 +0300

    feat(ApplicationCard.tsx): add team member role badge to application card view

--- studio/src/features/application/ApplicationCard.tsx
+++ studio/src/features/application/ApplicationCard.tsx
@@ -3,20 +3,28 @@ import { Application } from '@/types';
 import { DateTime } from 'luxon';
 import ApplicationSettings from './ApplicationSettings';
 import './application.scss';
+import useAuthStore from '@/store/auth/authStore';
 
 export default function ApplicationCard({ application }: { application: Application }) {
+	const { user } = useAuthStore();
 	return (
 		<div className='application-card'>
-			<div className='badge py-1 px-1.5 bg-elements-strong-purple inline-flex justify-center items-center'>
-				<span className='text-xs text-white font-sfCompact'>Developer</span>
-			</div>
-			<div className='flex items-center gap-4 mt-4 mb-2'>
+			<div className='flex items-center gap-4 flex-1'>
 				<Avatar size='lg' square>
 					<AvatarImage src={application.pictureUrl} />
 					<AvatarFallback name={application.name} color={application.color} />
 				</Avatar>
-				<div className='flex flex-col justify-center gap-2'>
-					<span className='text-xl text-default font-semibold'>{application.name}</span>
+				<div className='flex flex-col justify-center gap-2 flex-1'>
+					<div className='flex items-center justify-between'>
+						<span className='text-xl text-default font-semibold block truncate max-w-[12ch]'>
+							{application.name}
+						</span>
+						<div className='badge py-1 px-1.5 bg-elements-strong-purple inline-flex justify-center items-center'>
+							<span className='text-xs text-white font-sfCompact'>
+								{application.team.find((member) => member._id !== user?._id)?.role}
+							</span>
+						</div>
+					</div>
 					<div>
 						{application.team.map((member) => (
 							<Avatar key={member._id} size='xs'>
