58e7bf4cdbac0b0a1e4213d44df6885a8a71e174
Enes Malik Ozer <enes@altogic.com>
2023-06-19 20:21:12 +0300

    refactor(ApplicationCard.tsx): extract role variable from application.team.find() call feat(ApplicationCard.tsx): add translation support for 'created' text feat(ApplicationCard.tsx): add Badge component to display application role

--- studio/src/features/application/ApplicationCard.tsx
+++ studio/src/features/application/ApplicationCard.tsx
@@ -1,12 +1,17 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/Avatar';
+import useAuthStore from '@/store/auth/authStore';
 import { Application } from '@/types';
-import { DateTime } from 'luxon';
+import { getRelativeTime } from '@/utils/utils';
+import { ApplicationTeam } from '.';
 import ApplicationSettings from './ApplicationSettings';
 import './application.scss';
-import useAuthStore from '@/store/auth/authStore';
-
+import { useTranslation } from 'react-i18next';
+import { Badge } from '@/components/Badge';
+import { getApplicationRoleVariant } from '@/utils';
 export default function ApplicationCard({ application }: { application: Application }) {
 	const { user } = useAuthStore();
+	const { t } = useTranslation();
+	const role = application.team.find((member) => member._id !== user?._id)?.role;
 	return (
 		<div className='application-card'>
 			<div className='flex items-center gap-4 flex-1'>
@@ -19,25 +24,16 @@ export default function ApplicationCard({ application }: { application: Applicat
 						<span className='text-xl text-default font-semibold block truncate max-w-[12ch]'>
 							{application.name}
 						</span>
-						<div className='badge py-1 px-1.5 bg-elements-strong-purple inline-flex justify-center items-center'>
-							<span className='text-xs text-white font-sfCompact'>
-								{application.team.find((member) => member._id !== user?._id)?.role}
-							</span>
-						</div>
+						<Badge variant={getApplicationRoleVariant(role as string)}>{role}</Badge>
 					</div>
 					<div>
-						{application.team.map((member) => (
-							<Avatar key={member._id} size='xs'>
-								{/* <AvatarImage src={member?.pictureUrl} /> */}
-								<AvatarFallback name='Enes Malik Ozer' color='#123456' isUserAvatar />
-							</Avatar>
-						))}
+						<ApplicationTeam team={application.team} />
 					</div>
 				</div>
 			</div>
 			<div className=' flex items-center justify-between'>
 				<span className='text-subtle font-sfCompact text-xs'>
-					Create {DateTime.fromISO(application.createdAt).setLocale('en').toRelative()}
+					{t('general.created')} {getRelativeTime(application.createdAt)}
 				</span>
 				<ApplicationSettings />
 			</div>
