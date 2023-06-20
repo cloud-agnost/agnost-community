8747a15dd5a3aa4b32c203866bbcb61ce1937207
Özgür ÖZALP <ozgur@altogic.com>
2023-06-20 11:56:18 +0300

    add: main layout to user settings layout

--- studio/src/layouts/UserSettingsLayout/UserSettingsLayout.tsx
+++ studio/src/layouts/UserSettingsLayout/UserSettingsLayout.tsx
@@ -7,6 +7,7 @@ import { useTranslation } from 'react-i18next';
 import './UserSettingsLayout.scss';
 import useOrganizationStore from '@/store/organization/organizationStore.ts';
 import { MENU_ITEMS_FOR_PROFILE_SETTINGS } from '@/constants';
+import { Layout } from '@/layouts/Layout';
 
 type UserSettingsLayoutProps = {
 	children: ReactNode;
@@ -22,27 +23,29 @@ export default function UserSettingsLayout({
 	const { t } = useTranslation();
 	const currentOrgId = useOrganizationStore((state) => state.organization?._id);
 	return (
-		<div className='user-settings-layout'>
-			<div className='user-settings-layout-left'>
-				<div className='self-start'>
-					<Button
-						to={currentOrgId ? `/organization/${currentOrgId}` : '/organization'}
-						variant='text'
-						className='px-2 gap-4'
-					>
-						<ArrowLeft className='text-xl' />
-						<span>{t('profileSettings.back_to_app')}</span>
-					</Button>
-					<div className='font-semibold text-base leading-[26px] text-default'></div>
+		<Layout>
+			<div className='user-settings-layout'>
+				<div className='user-settings-layout-left'>
+					<div className='self-start'>
+						<Button
+							to={currentOrgId ? `/organization/${currentOrgId}` : '/organization'}
+							variant='text'
+							className='px-2 gap-4'
+						>
+							<ArrowLeft className='text-xl' />
+							<span>{t('profileSettings.back_to_app')}</span>
+						</Button>
+						<div className='font-semibold text-base leading-[26px] text-default'></div>
+					</div>
+					<Navbar items={MENU_ITEMS_FOR_PROFILE_SETTINGS(t)} />
 				</div>
-				<Navbar items={MENU_ITEMS_FOR_PROFILE_SETTINGS(t)} />
-			</div>
-			<div className='user-settings-layout-right'>
-				<div className='user-settings-layout-right-divider'>
-					<Description title={title}>{description}</Description>
+				<div className='user-settings-layout-right'>
+					<div className='user-settings-layout-right-divider'>
+						<Description title={title}>{description}</Description>
+					</div>
+					{children}
 				</div>
-				{children}
 			</div>
-		</div>
+		</Layout>
 	);
 }
