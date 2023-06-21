f78315f5a39e3fa5264c87c514cbef17e18f2101
Enes Malik Ozer <enes@altogic.com>
2023-06-21 14:07:57 +0300

    feat(ApplicationTable.tsx): add onRowClick event to open version modal when a row is clicked

--- studio/src/features/application/ApplicationTable/ApplicationTable.tsx
+++ studio/src/features/application/ApplicationTable/ApplicationTable.tsx
@@ -1,6 +1,7 @@
 import { DataTable } from '@/components/DataTable/DataTable';
 import { ApplicationColumns } from './ApplicationColumns';
 import { Application } from '@/types';
+import useOrganizationStore from '@/store/organization/organizationStore';
 
 interface ApplicationTableType {
 	apps: Application[];
@@ -9,7 +10,13 @@ interface ApplicationTableType {
 export default function ApplicationTable({ apps }: ApplicationTableType) {
 	return (
 		<div className='my-8'>
-			<DataTable columns={ApplicationColumns} data={apps} />
+			<DataTable
+				columns={ApplicationColumns}
+				data={apps}
+				onRowClick={() => {
+					useOrganizationStore.setState({ isVersionOpen: true });
+				}}
+			/>
 		</div>
 	);
 }
