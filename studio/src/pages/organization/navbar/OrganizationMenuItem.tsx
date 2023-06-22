5978064ca21abe8824b812d33902aa326c0074f2
Enes Malik Ozer <enes@altogic.com>
2023-06-22 12:51:46 +0300

    style(OrganizationMenuItem): add active class to li element and apply styles to active class in organizationMenu.scss

--- studio/src/pages/organization/navbar/OrganizationMenuItem.tsx
+++ studio/src/pages/organization/navbar/OrganizationMenuItem.tsx
@@ -13,8 +13,8 @@ interface OrganizationMenuItemProps {
 
 export default function OrganizationMenuItem({ item, active }: OrganizationMenuItemProps) {
 	return (
-		<li className='org-menu-item'>
-			<Link to={item.href} className={cn('org-menu-link', active && 'org-menu-link-active')}>
+		<li className={cn('org-menu-item', active && 'active')}>
+			<Link to={item.href} className='org-menu-link'>
 				<item.icon size={24} className='org-menu-icon' />
 				<span className='org-menu-item-name'>{item.name}</span>
 			</Link>
