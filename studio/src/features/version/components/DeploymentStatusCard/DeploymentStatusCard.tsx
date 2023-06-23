4d91a6cbad19c2d30b8b9a67072bccf2181f4e6c
Özgür ÖZALP <ozgur@altogic.com>
2023-06-23 17:13:35 +0300

    feat: version pages tabs and deployment status popup

--- studio/src/features/version/components/DeploymentStatusCard/DeploymentStatusCard.tsx
+++ studio/src/features/version/components/DeploymentStatusCard/DeploymentStatusCard.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
 import { cn } from '@/utils';
 import { Button } from 'components/Button';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import './deploymentStatusCard.scss';
 import { AuthUserAvatar } from 'components/AuthUserAvatar';
 import { useTranslation } from 'react-i18next';
+import { GearSix } from '@phosphor-icons/react';
+import { DeploymentSettings } from '@/features/version/components/DeploymentStatusCard/';
 
 interface DeploymentStatusCardProps {
 	triggerIcon: ReactNode;
@@ -13,14 +15,27 @@ interface DeploymentStatusCardProps {
 
 export default function DeploymentStatusCard({ triggerIcon }: DeploymentStatusCardProps) {
 	const { t } = useTranslation();
+	const [settingsIsOpen, setSettingsIsOpen] = useState(false);
 	return (
 		<DropdownMenu>
 			<DropdownMenuPrimitive.Trigger asChild>
 				<Button variant='blank'>{triggerIcon}</Button>
 			</DropdownMenuPrimitive.Trigger>
-			<DropdownMenuContent>
-				<DropdownMenuLabel>{t('version.deployment_status')}</DropdownMenuLabel>
+			<DropdownMenuContent className='overflow-hiddenr relative'>
+				<DropdownMenuLabel className='relative flex justify-between items-center'>
+					<span className='pr-10 truncate'>{t('version.deployment_status')}</span>
+					<Button
+						onClick={() => setSettingsIsOpen((prev) => !prev)}
+						variant='blank'
+						iconOnly
+						rounded
+						className='absolute hover:bg-subtle right-4 p-0 w-8 h-8'
+					>
+						<GearSix size={20} />
+					</Button>
+				</DropdownMenuLabel>
 				<div className='deployment-status-content'>
+					<DeploymentSettings isOpen={settingsIsOpen} close={() => setSettingsIsOpen(false)} />
 					<AuthUserAvatar size='xl' />
 					<p className='font-sfCompact text-sm text-default leading-6'>
 						{t('version.deployment_status_empty')}
