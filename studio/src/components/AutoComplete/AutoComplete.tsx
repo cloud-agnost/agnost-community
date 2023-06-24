c1f58c23cc36b7cd8d72ce7a532024aa8b570dc8
Enes Malik Ozer <enes@altogic.com>
2023-06-24 17:48:59 +0300

    feat(AutoComplete): add AutoComplete component
    The AutoComplete component is a reusable component that provides a search input field with suggestions. It receives two props, `onChange` and `loadOptions`. The `onChange` prop is a function that is called when a suggestion is selected. The `loadOptions` prop is a function that is called when the user types in the search input field. It returns a promise that resolves to an array of suggestions.
    
    The component is styled using SCSS. The styles are defined in the `autocomplete.scss` file. The component is exported from the `index.ts` file.

---
+++ studio/src/components/AutoComplete/AutoComplete.tsx
@@ -0,0 +1,23 @@
+import { OrganizationMember } from '@/types';
+import AsyncSelect from 'react-select/async';
+import './autocomplete.scss';
+
+interface Props {
+	onChange: (value: OrganizationMember) => void;
+	loadOptions: (inputValue: string) => void;
+}
+
+export default function AutoComplete({ onChange, loadOptions, ...props }: Props) {
+	return (
+		<AsyncSelect
+			cacheOptions
+			loadOptions={loadOptions}
+			onChange={(value) => onChange(value as OrganizationMember)}
+			defaultOptions
+			className='select-container'
+			classNamePrefix='select'
+			placeholder='Enter username or email'
+			{...props}
+		/>
+	);
+}
