0d76bc3887a921a5a539afd10b519999c704c717
Enes Malik Ozer <enes@altogic.com>
2023-06-24 17:47:32 +0300

    feat(OrganizationService.ts): add methods to change organization name, avatar, remove avatar, get organization members, transfer organization, and delete organization.

--- studio/src/services/OrganizationService.ts
+++ studio/src/services/OrganizationService.ts
@@ -1,6 +1,13 @@
 import { axios } from '@/helpers';
-import { Application, CreateApplicationResponse, Organization } from '@/types';
-import { CreateApplicationRequest } from '@/types';
+import {
+	Application,
+	CreateApplicationResponse,
+	Organization,
+	CreateApplicationRequest,
+	GetOrganizationMembersRequest,
+	ChangeOrganizationAvatarRequest,
+	OrganizationMember,
+} from '@/types';
 
 export default class OrganizationService {
 	static url = '/v1/org';
@@ -23,6 +30,58 @@ export default class OrganizationService {
 		).data;
 	}
 
+	static async changeOrganizationName(name: string, organizationId: string): Promise<Organization> {
+		return (
+			await axios.put(`${this.url}/${organizationId}`, {
+				name,
+			})
+		).data;
+	}
+
+	static async changeOrganizationAvatar(
+		req: ChangeOrganizationAvatarRequest,
+	): Promise<Organization> {
+		const formData = new FormData();
+		formData.append('picture', req.picture, req.picture.name);
+		return (
+			await axios.put(`${this.url}/${req.organizationId}/picture`, formData, {
+				headers: {
+					'Content-Type': 'multipart/form-data',
+				},
+			})
+		).data;
+	}
+	static async removeOrganizationAvatar(organizationId: string): Promise<Organization> {
+		return (await axios.delete(`${this.url}/${organizationId}/picture`)).data;
+	}
+
+	static async getOrganizationMembers(
+		req: GetOrganizationMembersRequest,
+	): Promise<OrganizationMember[]> {
+		const { page, size, role, sortBy, sortDir, search, organizationId, excludeSelf } = req;
+		return (
+			await axios.get(
+				`${this.url}/${organizationId}/member${excludeSelf ? '/exclude-current' : ''}`,
+				{
+					params: {
+						page,
+						size,
+						role,
+						sortBy,
+						sortDir,
+						search,
+					},
+				},
+			)
+		).data;
+	}
+
+	static async transferOrganization(organizationId: string, userId: string) {
+		return (await axios.post(`${this.url}/${organizationId}/transfer/${userId}`, { userId })).data;
+	}
+	static async deleteOrganization(organizationId: string) {
+		return (await axios.delete(`${this.url}/${organizationId}`, { data: {} })).data;
+	}
 	static async getOrganizationApps(organizationId: string): Promise<Application[]> {
 		return (await axios.get(`${this.url}/${organizationId}/app`)).data;
 	}
--- studio/src/store/organization/organizationStore.ts
+++ studio/src/store/organization/organizationStore.ts
@@ -8,7 +8,13 @@ import {
 	CreateApplicationRequest,
 	CreateApplicationResponse,
 	DeleteApplicationRequest,
+	ChangeOrganizationAvatarRequest,
+	ChangeOrganizationNameRequest,
+	GetOrganizationMembersRequest,
+	OrganizationMember,
+	TransferOrganizationRequest,
 } from '@/types';
+import { BaseRequest } from '@/types/type';
 import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
 interface OrganizationStore {
@@ -18,10 +24,16 @@ interface OrganizationStore {
 	applications: Application[];
 	temp: Application[];
 	isVersionOpen: boolean;
+	members: OrganizationMember[];
 	getAllOrganizationByUser: () => Promise<Organization[] | APIError>;
 	createOrganization: (req: CreateOrganizationRequest) => Promise<Organization | APIError>;
 	selectOrganization: (organization: Organization) => void;
 	leaveOrganization: (req: LeaveOrganizationRequest) => Promise<void>;
+	changeOrganizationName: (req: ChangeOrganizationNameRequest) => Promise<Organization>;
+	changeOrganizationAvatar: (req: ChangeOrganizationAvatarRequest) => Promise<Organization>;
+	removeOrganizationAvatar: (req: BaseRequest) => Promise<Organization>;
+	transferOrganization: (req: TransferOrganizationRequest) => Promise<Organization>;
+	deleteOrganization: (req: BaseRequest) => Promise<void>;
 	getOrganizationApps: (organizationId: string) => Promise<Application[] | APIError>;
 	createApplication: (
 		req: CreateApplicationRequest,
@@ -29,6 +41,7 @@ interface OrganizationStore {
 	searchApplications: (query: string) => Promise<Application[] | APIError>;
 	deleteApplication: (req: DeleteApplicationRequest) => Promise<void>;
 	leaveAppTeam: (req: DeleteApplicationRequest) => Promise<void>;
+	getOrganizationMembers: (req: GetOrganizationMembersRequest) => Promise<OrganizationMember[]>;
 }
 
 const useOrganizationStore = create<OrganizationStore>()(
@@ -41,6 +54,7 @@ const useOrganizationStore = create<OrganizationStore>()(
 				applications: [],
 				temp: [],
 				isVersionOpen: false,
+				members: [],
 				getAllOrganizationByUser: async () => {
 					try {
 						set({ loading: true });
@@ -91,6 +105,105 @@ const useOrganizationStore = create<OrganizationStore>()(
 						throw error as APIError;
 					}
 				},
+				changeOrganizationName: async ({
+					name,
+					organizationId,
+					onSuccess,
+					onError,
+				}: ChangeOrganizationNameRequest) => {
+					try {
+						const res = await OrganizationService.changeOrganizationName(name, organizationId);
+						set({
+							organizations: get().organizations.map((organization) => {
+								if (organization._id === organizationId) {
+									return res;
+								}
+								return organization;
+							}),
+							organization: res,
+						});
+						if (onSuccess) onSuccess();
+						return res;
+					} catch (error) {
+						if (onError) onError(error as APIError);
+						throw error as APIError;
+					}
+				},
+				changeOrganizationAvatar: async (req: ChangeOrganizationAvatarRequest) => {
+					try {
+						const res = await OrganizationService.changeOrganizationAvatar(req);
+						set({
+							organization: res,
+						});
+						if (req.onSuccess) req.onSuccess();
+						return res;
+					} catch (error) {
+						if (req.onError) req.onError(error as APIError);
+						throw error as APIError;
+					}
+				},
+				removeOrganizationAvatar: async (req: BaseRequest) => {
+					try {
+						const res = await OrganizationService.removeOrganizationAvatar(
+							get()?.organization?._id as string,
+						);
+						set({
+							organization: res,
+						});
+						if (req.onSuccess) req.onSuccess();
+						return res;
+					} catch (error) {
+						if (req.onError) req.onError(error as APIError);
+						throw error as APIError;
+					}
+				},
+				transferOrganization: async (req: TransferOrganizationRequest) => {
+					try {
+						const res = await OrganizationService.transferOrganization(
+							req.organizationId,
+							req.userId,
+						);
+						set({
+							organizations: get().organizations.filter(
+								(organization) => organization._id !== get()?.organization?._id,
+							),
+							organization: null,
+						});
+						if (req.onSuccess) req.onSuccess();
+						return res;
+					} catch (error) {
+						if (req.onError) req.onError(error as APIError);
+						throw error as APIError;
+					}
+				},
+				getOrganizationMembers: async (req: GetOrganizationMembersRequest) => {
+					try {
+						const res = await OrganizationService.getOrganizationMembers(req);
+						set({
+							members: res,
+						});
+						if (req.onSuccess) req.onSuccess();
+						return res;
+					} catch (error) {
+						if (req.onError) req.onError(error as APIError);
+						throw error as APIError;
+					}
+				},
+				deleteOrganization: async (req: BaseRequest) => {
+					try {
+						await OrganizationService.deleteOrganization(get()?.organization?._id as string);
+						set({
+							organizations: get().organizations.filter(
+								(organization) => organization._id !== get()?.organization?._id,
+							),
+							organization: null,
+						});
+						if (req.onSuccess) req.onSuccess();
+					} catch (error) {
+						if (req.onError) req.onError(error as APIError);
+						throw error as APIError;
+					}
+				},
 				getOrganizationApps: async (organizationId: string) => {
 					try {
 						set({ loading: true });
@@ -110,13 +223,19 @@ const useOrganizationStore = create<OrganizationStore>()(
 					onError,
 				}: CreateApplicationRequest) => {
 					try {
+						set({ loading: true });
 						const res = await OrganizationService.createApplication({ orgId, name });
 						if (onSuccess) onSuccess();
-						set({ applications: [...get().applications, res.app] });
+						set({
+							applications: [...get().applications, res.app],
+							temp: [...get().applications, res.app],
+						});
 						return res;
 					} catch (error) {
 						if (onError) onError(error as APIError);
 						throw error as APIError;
+					} finally {
+						set({ loading: false });
 					}
 				},
 				searchApplications: async (query: string) => {
@@ -147,6 +266,7 @@ const useOrganizationStore = create<OrganizationStore>()(
 						await OrganizationService.deleteApplication(appId, orgId);
 						set({
 							applications: get().applications.filter((app) => app._id !== appId),
+							temp: [...get().temp.filter((app) => app._id !== appId)],
 						});
 						if (onSuccess) onSuccess();
 					} catch (error) {
@@ -159,6 +279,7 @@ const useOrganizationStore = create<OrganizationStore>()(
 						await OrganizationService.leaveAppTeam(appId, orgId);
 						set({
 							applications: get().applications.filter((app) => app._id !== appId),
+							temp: [...get().temp.filter((app) => app._id !== appId)],
 						});
 						if (onSuccess) onSuccess();
 					} catch (error) {
