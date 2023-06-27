d1ac0b44f841a3cc7e105b866a715ec47197bcf3
Enes Malik Ozer <enes@altogic.com>
2023-06-27 21:55:53 +0300

    Refactor organization imports to lowercase

--- studio/src/features/Organization/Settings/Members/OrganizationInvitationTable.tsx
+++ studio/src/features/Organization/Settings/Members/OrganizationInvitationTable.tsx
@@ -1,6 +1,6 @@
 import { DataTable } from '@/components/DataTable';
 import { InfiniteScroll } from '@/components/InfiniteScroll';
-import { OrganizationInvitationsColumns } from '@/features/Organization';
+import { OrganizationInvitationsColumns } from '@/features/organization';
 import useOrganizationStore from '@/store/organization/organizationStore';
 import { OrganizationInvitations } from '@/types';
 import { Row } from '@tanstack/react-table';
