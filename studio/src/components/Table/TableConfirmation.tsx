d384d4f9af61ba27bd617463f2eb2b7a29c97c36
Enes Malik Ozer <enes@altogic.com>
2023-06-27 14:32:55 +0300

    Add TableConfirmation component

---
+++ studio/src/components/Table/TableConfirmation.tsx
@@ -0,0 +1,59 @@
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/Popover';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button } from '../Button';
+import { X, WarningCircle } from '@phosphor-icons/react';
+import { Avatar, AvatarFallback } from '../Avatar';
+interface TableConfirmationProps {
+	children: React.ReactNode;
+	onConfirm: () => void;
+	title: string;
+	description: string;
+}
+
+export function TableConfirmation({
+	children,
+	onConfirm,
+	title,
+	description,
+}: TableConfirmationProps) {
+	const { t } = useTranslation();
+	const [open, setOpen] = useState(false);
+	return (
+		<Popover open={open} onOpenChange={setOpen}>
+			<PopoverTrigger>{children}</PopoverTrigger>
+			<PopoverContent className='mr-2'>
+				<div id='popup-modal' tabIndex={-1}>
+					<div className='relative w-full max-w-sm max-h-full'>
+						<Button
+							variant='blank'
+							className='absolute top-3 right-2.5 text-icon-base hover:text-icon-secondary rounded-full'
+							data-modal-hide='popup-modal'
+							onClick={() => setOpen(false)}
+						>
+							<X size={20} />
+							<span className='sr-only'>Close modal</span>
+						</Button>
+						<div className='relative rounded p-4'>
+							<div className='flex flex-col justify-center items-center space-y-4 text-center'>
+								<Avatar size='3xl'>
+									<AvatarFallback color='#9B7B0866' />
+								</Avatar>
+								<h3 className='text-lg font-semibold text-default'>{title}</h3>
+								<p className='text-sm font-normal text-subtle'>{description}</p>
+							</div>
+							<div className='flex  items-center justify-center gap-4 mt-8'>
+								<Button variant='text' size='lg' onClick={() => setOpen(false)}>
+									{t('general.cancel')}
+								</Button>
+								<Button variant='primary' size='lg' onClick={onConfirm}>
+									{t('general.ok')}
+								</Button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</PopoverContent>
+		</Popover>
+	);
+}
