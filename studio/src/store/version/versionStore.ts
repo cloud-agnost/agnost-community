f94545f888b51162b2df47bae69340e685b96e32
Enes Malik Ozer <enes@altogic.com>
2023-07-04 00:24:57 +0300

    Refactor version store to handle paginated versions

--- studio/src/store/version/versionStore.ts
+++ studio/src/store/version/versionStore.ts
@@ -1,12 +1,13 @@
-import { APIError, GetVersionRequest, Version, Tab } from '@/types';
-import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
 import { VersionService } from '@/services';
+import { APIError, GetVersionRequest, Tab, Version } from '@/types';
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 interface VersionStore {
 	loading: boolean;
 	error: APIError | null;
 	versions: Version[];
+	versionPage: number;
 	tabs: Tab[];
 	getAllVersionsVisibleToUser: (req: GetVersionRequest) => Promise<void>;
 	removeTab: (id: string) => string | undefined;
@@ -14,66 +15,66 @@ interface VersionStore {
 	getTabByPath: (path: string) => Tab | undefined;
 	getTabById: (id: string) => Tab | undefined;
 	getPreviousTab: (currentTabId: string) => Tab | undefined;
+	setVersionPage: (page: number) => void;
 }
 
 const useVersionStore = create<VersionStore>()(
 	devtools(
-		persist(
-			(set, get) => ({
-				loading: false,
-				error: null,
-				versions: [],
-				tabs: [],
-				getAllVersionsVisibleToUser: async (req: GetVersionRequest) => {
-					set({ loading: true });
-					try {
-						const versions = await VersionService.getAllVersionsVisibleToUser(req);
-						set({ versions });
-					} catch (error) {
-						set({ error: error as APIError });
-					} finally {
-						set({ loading: false });
-					}
-				},
-				removeTab: (id: string) => {
-					const prevPath = get().getPreviousTab(id)?.path;
-
-					set((state) => ({
-						tabs: state.tabs.filter((tab) => tab.id !== id),
-					}));
-
-					return prevPath;
-				},
-				addTab: (tab) => {
-					if (get().getTabByPath(tab.path)) return;
-
-					const newTab = tab as Tab;
-					newTab.id = crypto.randomUUID();
-
-					set((state) => {
-						return {
-							tabs: [...state.tabs, newTab],
-						};
-					});
-				},
-				getTabByPath: (path: string) => {
-					return get().tabs.find((tab) => tab.path === path);
-				},
-				getTabById: (id: string) => {
-					return get().tabs.find((tab) => tab.id === id);
-				},
-				getPreviousTab: (currentTabId: string) => {
-					const currentTabIndex = get().tabs.findIndex((tab) => tab.id === currentTabId);
-					if (currentTabIndex === -1) return;
-
-					return get().tabs[currentTabIndex - 1];
-				},
-			}),
-
-			{
-				name: 'version-storage',
+		(set, get) => ({
+			loading: false,
+			error: null,
+			versions: [],
+			versionPage: 0,
+			tabs: [],
+			getAllVersionsVisibleToUser: async (req: GetVersionRequest) => {
+				set({ loading: true });
+				try {
+					const versions = await VersionService.getAllVersionsVisibleToUser(req);
+					if (!get().versionPage) set({ versions });
+					else set((prev) => ({ versions: [...prev.versions, ...versions] }));
+				} catch (error) {
+					set({ error: error as APIError });
+				} finally {
+					set({ loading: false });
+				}
 			},
-		),
+			removeTab: (id: string) => {
+				const prevPath = get().getPreviousTab(id)?.path;
+
+				set((state) => ({
+					tabs: state.tabs.filter((tab) => tab.id !== id),
+				}));
+
+				return prevPath;
+			},
+			addTab: (tab) => {
+				if (get().getTabByPath(tab.path)) return;
+
+				const newTab = tab as Tab;
+				newTab.id = crypto.randomUUID();
+
+				set((state) => {
+					return {
+						tabs: [...state.tabs, newTab],
+					};
+				});
+			},
+			getTabByPath: (path: string) => {
+				return get().tabs.find((tab) => tab.path === path);
+			},
+			getTabById: (id: string) => {
+				return get().tabs.find((tab) => tab.id === id);
+			},
+			getPreviousTab: (currentTabId: string) => {
+				const currentTabIndex = get().tabs.findIndex((tab) => tab.id === currentTabId);
+				if (currentTabIndex === -1) return;
+
+				return get().tabs[currentTabIndex - 1];
+			},
+			setVersionPage: (page: number) => {
+				set({ versionPage: page });
+			},
+		}),
 		{
 			name: 'version-storage',
 		},
