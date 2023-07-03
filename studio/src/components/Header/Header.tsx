3b37e266a97d7b9df51c786c6e112f2d453cde5d
Özgür ÖZALP <ozgur@altogic.com>
2023-07-03 23:11:44 +0300

    working on version page

--- studio/src/components/Header/Header.tsx
+++ studio/src/components/Header/Header.tsx
@@ -1,16 +1,14 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/Avatar';
 import { Button } from '@/components/Button';
 import { AgnostOnlyLogo } from '@/components/icons';
-import useAuthStore from '@/store/auth/authStore.ts';
 import { Bell, Cloud } from '@phosphor-icons/react';
 import { Link } from 'react-router-dom';
 import './header.scss';
 import { MENU_ITEMS } from '@/constants';
 import { OrganizationDropdown } from '@/features/Organization/OrganizationDropdown';
 import { DeploymentStatusCard } from '@/features/version/components/DeploymentStatusCard';
+import { AuthUserAvatar } from 'components/AuthUserAvatar';
 
 export function Header() {
-	const { user } = useAuthStore();
 	return (
 		<header className='header-menu'>
 			<div className='header-menu-left'>
@@ -43,10 +41,7 @@ export function Header() {
 					</div>
 					<div className='header-menu-right-actions-user'>
 						<Link to='/profile/settings' className='header-menu-right-actions-user-avatar'>
-							<Avatar size='sm'>
-								<AvatarImage src='https://avatars.githubusercontent.com/u/1500684?s=400&u=3a3d2f8d7d1b5c2b5f5d9f3e3b9e5f6f8b0c9b8a&v=4' />
-								{user && <AvatarFallback color={user?.color} name={user?.name} />}
-							</Avatar>
+							<AuthUserAvatar size='sm' />
 						</Link>
 					</div>
 				</div>
