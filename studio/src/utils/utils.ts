53110c895e2eb296a4dd77d547f87f649e9037a9
Enes Malik Ozer <enes@altogic.com>
2023-07-04 11:33:45 +0300

    Refactor isArray function to use generic type

--- studio/src/utils/utils.ts
+++ studio/src/utils/utils.ts
@@ -107,6 +107,6 @@ export function isEmpty(value: unknown): boolean {
 
 	return false;
 }
-export function isArray(value: unknown): value is any[] {
+export function isArray<T>(value: unknown): value is T[] {
 	return Array.isArray(value);
 }
