de2cd039c95b3c3c2898b6c144da65c7f1a60913
Enes Malik Ozer <enes@altogic.com>
2023-07-06 16:18:08 +0300

    Add useEffect hook to fetch types if they are not already loaded

--- studio/src/App.tsx
+++ studio/src/App.tsx
@@ -4,9 +4,18 @@ import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { router } from '@/router';
+import { useEffect } from 'react';
+import useTypeStore from './store/types/typeStore';
 
 function App() {
 	useRenewToken(2);
+	const { getAllTypes, isTypesOk } = useTypeStore();
+
+	useEffect(() => {
+		if (!isTypesOk) {
+			getAllTypes();
+		}
+	}, [isTypesOk]);
 	return (
 		<>
 			<RouterProvider router={router} />
