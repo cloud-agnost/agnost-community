3bdcd76d593b94be2b299b9ca6d4c4c4f2da5d5b
Enes Malik Ozer <enes@altogic.com>
2023-07-07 14:27:54 +0300

    Refactor RoleSelect component to include a disabled prop

--- studio/src/components/RoleDropdown/RoleSelect.tsx
+++ studio/src/components/RoleDropdown/RoleSelect.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from 'components/Select';
 import useTypeStore from '@/store/types/typeStore';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from 'components/Select';
 
 interface RoleSelectProps {
 	role: string;
 	type: 'app' | 'org';
 	onSelect: (role: string) => void;
+	disabled?: boolean;
 }
-function RoleSelect({ role, type, onSelect, ...props }: RoleSelectProps) {
+function RoleSelect({ role, type, disabled, onSelect }: RoleSelectProps) {
 	const { appRoles, orgRoles } = useTypeStore();
 	const roles = type === 'app' ? appRoles : orgRoles;
 	return (
-		<Select defaultValue={role} onValueChange={onSelect} {...props}>
+		<Select defaultValue={role} onValueChange={onSelect} disabled={disabled}>
 			<SelectTrigger className='w-[120px]'>
 				<SelectValue>{role}</SelectValue>
 			</SelectTrigger>
