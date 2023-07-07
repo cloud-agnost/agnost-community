eaadcb1910a551fba4347471177cba5e1ba634d2
Enes Malik Ozer <enes@altogic.com>
2023-07-07 14:09:36 +0300

    Add infinite scroll to AppInvitations

--- studio/src/features/application/Settings/Invitations/AppInvitations.tsx
+++ studio/src/features/application/Settings/Invitations/AppInvitations.tsx
@@ -5,19 +5,35 @@ import { AppInvitationsColumns } from './AppInvitationsColumns';
 import { Invitation } from '@/types';
 import AppInvitationFilter from './AppInvitationFilter';
 import { Row } from '@tanstack/react-table';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { ScrollArea } from 'components/ScrollArea';
 
 function AppInvitations() {
 	const { invitations } = useApplicationStore();
 	const [selectedRows, setSelectedRows] = useState<Row<Invitation>[]>();
 
 	return (
-		<div className='space-y-4'>
-			<AppInvitationFilter selectedRows={selectedRows} />
-			<DataTable<Invitation>
-				columns={AppInvitationsColumns}
-				data={invitations}
-				setSelectedRows={setSelectedRows}
-			/>
+		<div className='p-6 max-h-[85%] overflow-auto' id='invitation-infinite-scroll'>
+			<InfiniteScroll
+				scrollableTarget='invitation-infinite-scroll'
+				next={() => {
+					useApplicationStore.setState?.({
+						invitationPage: useApplicationStore.getState?.().invitationPage + 1,
+					});
+				}}
+				hasMore={true}
+				loader={<></>}
+				dataLength={invitations.length}
+			>
+				<div className='space-y-4'>
+					<AppInvitationFilter selectedRows={selectedRows} />
+					<DataTable<Invitation>
+						columns={AppInvitationsColumns}
+						data={invitations}
+						setSelectedRows={setSelectedRows}
+					/>
+				</div>
+			</InfiniteScroll>
 		</div>
 	);
 }
