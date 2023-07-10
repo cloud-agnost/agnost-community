53c41afc4748854e014ece583268cf2f8a97c1ba
Enes Malik Ozer <enes@altogic.com>
2023-07-10 12:13:26 +0300

    Refactor OrganizationSelect component to fetch organizations only when the user is authenticated

--- studio/src/pages/organization/OrganizationSelect.tsx
+++ studio/src/pages/organization/OrganizationSelect.tsx
@@ -9,18 +9,19 @@ import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { useOutletContext } from 'react-router-dom';
 import './organization.scss';
+import { useEffect } from 'react';
 
 async function loader() {
-	if (!useAuthStore.getState().isAuthenticated()) return null;
-	return useOrganizationStore.getState().getAllOrganizationByUser();
+	return null;
 }
 
 export default function OrganizationSelect() {
-	const { organizations, selectOrganization } = useOrganizationStore();
+	const { organizations, selectOrganization, getAllOrganizationByUser } = useOrganizationStore();
+	const { user, isAuthenticated } = useAuthStore();
 	const { openOrgCreateModal } = useOutletContext<{
 		openOrgCreateModal: () => void;
 	}>();
-	const { user } = useAuthStore();
+
 	const navigate = useNavigate();
 	const { t } = useTranslation();
 
@@ -28,6 +29,12 @@ export default function OrganizationSelect() {
 		selectOrganization(org);
 		navigate(`/organization/${org?._id}`);
 	}
+
+	useEffect(() => {
+		if (isAuthenticated()) {
+			getAllOrganizationByUser();
+		}
+	}, [user?._id, isAuthenticated]);
 
 	return (
 		<div>
