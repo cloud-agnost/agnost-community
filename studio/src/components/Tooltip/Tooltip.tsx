7e9bf6563f419d366e44a5686b2fcb3ac994bd80
Enes Malik Ozer <enes@altogic.com>
2023-07-12 11:30:41 +0300

    Update tooltip background color and text color

--- studio/src/components/Tooltip/Tooltip.tsx
+++ studio/src/components/Tooltip/Tooltip.tsx
@@ -35,7 +35,7 @@ const TooltipContent = React.forwardRef<
 		ref={ref}
 		sideOffset={sideOffset}
 		className={cn(
-			'z-50 overflow-hidden rounded-md bg-subtle px-3 py-1.5 text-xs text-subtle animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-10',
+			'z-50 overflow-hidden rounded-md bg-wrapper-background-light px-3 py-1.5 text-xs text-default animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-10',
 			className,
 		)}
 		{...props}
