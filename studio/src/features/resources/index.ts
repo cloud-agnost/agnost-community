b1adc653292a3554da5f2c0ad8fd97046650c3b1
Enes Malik Ozer <enes@altogic.com>
2023-07-18 13:00:35 +0300

    add connect and create cache to resource

--- studio/src/features/resources/index.ts
+++ studio/src/features/resources/index.ts
@@ -1,7 +1,13 @@
+export { default as ConnectCache } from './CreateResource/Cache/ConnectCache';
+export { default as CreateCache } from './CreateResource/Cache/CreateCache';
 export { default as CreateResource } from './CreateResource/CreateResource';
 export { default as ConnectDatabase } from './CreateResource/Database/ConnectDatabase/ConnectDatabase';
 export { default as CreateDatabase } from './CreateResource/Database/CreateDatabase';
+export { default as ConnectQueue } from './CreateResource/Queue/ConnectQueue';
+export { default as CreateQueue } from './CreateResource/Queue/CreateQueue';
 export { default as ConnectAWS } from './CreateResource/Storage/ConnectAWS';
+export { default as ConnectAzure } from './CreateResource/Storage/ConnectAzure';
+export { default as ConnectGCP } from './CreateResource/Storage/ConnectGCP';
 export { default as OrgResources } from './OrgResources';
 export { default as ResourceTable } from './ResourceTable/ResourceTable';
 export { default as SelectResourceType } from './SelectResourceType';
