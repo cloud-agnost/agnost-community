af0f3c2c2af8e5b958c113e8df91878e74135341
Enes Malik Ozer <enes@altogic.com>
2023-07-18 13:02:55 +0300

    add mongo connection options

--- studio/src/features/resources/CreateResource/Database/ConnectDatabase/ReadReplicas.tsx
+++ studio/src/features/resources/CreateResource/Database/ConnectDatabase/ReadReplicas.tsx
@@ -9,21 +9,27 @@ import {
 	DialogTrigger,
 } from '@/components/Dialog';
 import { Form } from '@/components/Form';
-import { Pencil } from '@/components/icons';
+import { Pencil, TestConnection } from '@/components/icons';
 import { AccessDbSchema, ConnectDatabaseSchema } from '@/types';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Plus, Trash } from '@phosphor-icons/react';
-import { FormEvent, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm, useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import * as z from 'zod';
 import CreateResourceItem from '../../../CreateResourceItem';
 import DatabaseInfo from './DatabaseInfo';
-
+import MongoConnectionFormat from './MongoConnectionFormat';
+import useResourceStore from '@/store/resources/resourceStore';
+import { useToast } from '@/hooks';
+import { INSTANCE_PORT_MAP } from '@/constants';
 export default function ReadReplicas() {
 	const { t } = useTranslation();
+	const { notify } = useToast();
+	const { testExistingResourceConnection } = useResourceStore();
+	const [loading, setLoading] = useState(false);
 	const [openModal, setOpenModal] = useState(false);
-	const { setValue, getValues, reset, handleSubmit } =
+	const { setValue, getValues, reset, watch } =
 		useFormContext<z.infer<typeof ConnectDatabaseSchema>>();
 	const readReplicas = getValues('accessReadOnly') ?? [];
 	const replicasForm = useForm<z.infer<typeof AccessDbSchema>>({
@@ -37,8 +43,6 @@ export default function ReadReplicas() {
 	}
 
 	function onSubmit() {
-		// e.preventDefault();
-		// e.stopPropagation;
 		replicasForm.handleSubmit(addNewReplica)();
 	}
 
@@ -57,6 +61,38 @@ export default function ReadReplicas() {
 		});
 		setOpenModal(true);
 	}
+	function testResourceConnection() {
+		setLoading(true);
+		testExistingResourceConnection({
+			access: {
+				...replicasForm.getValues(),
+			},
+			instance: getValues('instance'),
+			allowedRoles: getValues('allowedRoles'),
+			type: 'database',
+			onSuccess: () => {
+				setLoading(false);
+				notify({
+					title: t('general.success'),
+					description: t('resources.database.test_success'),
+					type: 'success',
+				});
+			},
+			onError: ({ error, details }) => {
+				setLoading(false);
+				notify({
+					title: error,
+					description: details,
+					type: 'error',
+				});
+			},
+		});
+	}
+	useEffect(() => {
+		if (watch('instance')) {
+			replicasForm.setValue('port', INSTANCE_PORT_MAP[watch('instance')]);
+		}
+	}, [watch('instance')]);
 	return (
 		<CreateResourceItem title={t('resources.database.add_replica')}>
 			<div className='space-y-3'>
@@ -83,9 +119,9 @@ export default function ReadReplicas() {
 			</div>
 			<Dialog open={openModal} onOpenChange={setOpenModal}>
 				<DialogTrigger asChild>
-					<Button type='button' variant='blank'>
-						<Plus size={16} className='text-icon-secondary' />
-						<span className='ml-2'>{t('resources.database.add_replica')}</span>
+					<Button type='button' variant='text'>
+						<Plus size={16} className='text-brand-primary' />
+						<span className='ml-2 text-brand-primary'>{t('resources.database.add_replica')}</span>
 					</Button>
 				</DialogTrigger>
 				<DialogContent>
@@ -94,18 +130,27 @@ export default function ReadReplicas() {
 					</DialogHeader>
 					<Form {...replicasForm}>
 						<form className='space-y-4'>
-							<DatabaseInfo
-								control={replicasForm.control}
-								errors={replicasForm.formState.errors}
-								modal
-							/>
+							{watch('instance') === 'MongoDB' && <MongoConnectionFormat />}
+							<DatabaseInfo modal />
 
 							<DialogFooter>
-								<div className='flex justify-end mt-8'>
+								<div className='flex justify-end gap-4 mt-8'>
+									<Button
+										variant='outline'
+										loading={loading}
+										onClick={testResourceConnection}
+										type='button'
+										size='lg'
+									>
+										<TestConnection className='w-4 h-4 text-icon-default mr-2' />
+										{t('resources.database.test')}
+									</Button>
 									<DialogClose asChild>
-										<Button variant='secondary'>{t('general.cancel')}</Button>
+										<Button size='lg' variant='secondary'>
+											{t('general.cancel')}
+										</Button>
 									</DialogClose>
-									<Button type='button' className='ml-2' onClick={onSubmit}>
+									<Button size='lg' type='button' onClick={onSubmit}>
 										{t('resources.database.add_replica')}
 									</Button>
 								</div>
