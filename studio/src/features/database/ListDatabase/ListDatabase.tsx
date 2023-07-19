bc86f1a7f55a659e01b6ebd6dfba84f74e975f06
Özgür ÖZALP <ozgur@altogic.com>
2023-07-19 10:59:21 +0300

    Update ListDatabase.tsx

--- studio/src/features/database/ListDatabase/ListDatabase.tsx
+++ studio/src/features/database/ListDatabase/ListDatabase.tsx
@@ -17,7 +17,6 @@ export default function ListDatabase() {
 	const {
 		toDeleteDatabase,
 		searchDatabases,
-		toEditDatabase,
 		isOpenDeleteDatabaseDialog,
 		setIsOpenDeleteDatabaseDialog,
 		databasesForSearch,
