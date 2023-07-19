062b1a9eecebb44c0f084bcb2b90e07dc27001b3
Enes Malik Ozer <enes@altogic.com>
2023-07-19 12:02:51 +0300

    Add delete functionality and display selected row count

--- studio/src/components/Table/SelectedRowDropdown.tsx
+++ studio/src/components/Table/SelectedRowDropdown.tsx
@@ -1,42 +1,39 @@
-import { Trash } from '@phosphor-icons/react';
+import { Minus, Trash } from '@phosphor-icons/react';
+import { Table } from '@tanstack/react-table';
 import { Button } from 'components/Button';
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from 'components/Dropdown';
 import { useTranslation } from 'react-i18next';
 
 interface Props {
 	onDelete: () => void;
 	selectedRowLength: number;
+	table: Table<any>;
 }
-function SelectedRowDropdown({ onDelete, selectedRowLength }: Props) {
+function SelectedRowDropdown({ onDelete, selectedRowLength, table }: Props) {
 	const { t } = useTranslation();
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant='outline'>
-					{selectedRowLength > 0 ? (
-						<span>
-							{t('general.selected', {
-								count: selectedRowLength,
-							})}
-						</span>
-					) : (
-						<span>{t('general.actions')}</span>
-					)}
+	return selectedRowLength > 0 ? (
+		<div className='flex items-center  border border-border rounded-md bg-lighter'>
+			<div className='flex items-center gap-2 border-r border-button-border p-1.5'>
+				<Button
+					size='sm'
+					variant='primary'
+					className=' bg-button-primary h-1/2 px-1'
+					onClick={() => table?.resetRowSelection()}
+				>
+					<Minus size={16} weight='bold' className='text-icon-secondary' />
 				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent>
-				<DropdownMenuItem onClick={onDelete}>
-					<Trash size={16} className='members-filter-icon' />
-					<span>{t('general.delete_all')}</span>
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
-	);
+
+				<span className='font-sfCompact text-sm text-elements-blue'>
+					{t('general.selected', {
+						count: selectedRowLength,
+					})}
+				</span>
+			</div>
+
+			<Button variant='blank' iconOnly onClick={onDelete}>
+				<Trash size={20} className='text-icon-base' />
+			</Button>
+		</div>
+	) : null;
 }
 
 export default SelectedRowDropdown;
