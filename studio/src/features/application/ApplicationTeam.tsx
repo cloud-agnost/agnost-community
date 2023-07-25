cd3f55555cf54c81edec6b2e197c0ed170b4afc5
Özgür ÖZALP <ozgur@altogic.com>
2023-07-25 14:03:54 +0300

    fix: null safety

--- studio/src/features/application/ApplicationTeam.tsx
+++ studio/src/features/application/ApplicationTeam.tsx
@@ -8,13 +8,13 @@ interface ApplicationTeamProps {
 export default function ApplicationTeam({ team, table = false }: ApplicationTeamProps) {
 	return (
 		<div className='application-team'>
-			{team.slice(0, 4).map((member) => (
+			{team?.slice(0, 4)?.map((member) => (
 				<Avatar key={member._id} size={table ? 'xs' : 'sm'}>
 					<AvatarImage src={member.userId.pictureUrl} />
 					<AvatarFallback name={member.userId.name} color={member.userId.color} isUserAvatar />
 				</Avatar>
 			))}
-			{team.length > 4 && (
+			{team?.length > 4 && (
 				<Avatar size='xs'>
 					<AvatarFallback name={`${team.length - 4}+`} color='#fff' />
 				</Avatar>
