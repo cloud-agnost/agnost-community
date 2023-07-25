72d1f21a7fc12f0d4d13fd6a44c587729d7532fc
Özgür ÖZALP <ozgur@altogic.com>
2023-07-25 14:04:51 +0300

    Update OrganizationDetails.tsx

--- studio/src/pages/organization/OrganizationDetails.tsx
+++ studio/src/pages/organization/OrganizationDetails.tsx
@@ -1,11 +1,12 @@
 import ApplicationCreateModal from '@/features/application/ApplicationCreateModal';
 import { Layout } from '@/layouts/Layout';
 import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useMatch } from 'react-router-dom';
 import { OrganizationMenu } from '@/features/organization';
 
 export default function OrganizationDetails() {
 	const [openAppCreateModal, setOpenAppCreateModal] = useState(false);
+	const hideTabMenu = useMatch('/organization/:orgId/apps/:appId/*');
 	return (
 		<Layout>
 			<ApplicationCreateModal
@@ -13,9 +14,11 @@ export default function OrganizationDetails() {
 				isOpen={openAppCreateModal}
 				closeModal={() => setOpenAppCreateModal(false)}
 			/>
-			<div className='org-menu-container'>
-				<OrganizationMenu />
-			</div>
+			{!hideTabMenu && (
+				<div className='org-menu-container'>
+					<OrganizationMenu />
+				</div>
+			)}
 			<Outlet
 				context={{
 					openAppCreateModal: () => setOpenAppCreateModal(true),
