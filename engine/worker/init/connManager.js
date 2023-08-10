6a3843260193f5339b676df07937d459dc0057ea
Özgür ÖZALP <ozgur@altogic.com>
2023-08-11 02:52:15 +0300

    Update connManager.js

--- engine/worker/init/connManager.js
+++ engine/worker/init/connManager.js
@@ -8,121 +8,116 @@ import { DATABASE } from "../config/constants.js";
  * Pools the connections to the database
  */
 class ConnectionManager {
-	constructor() {
-		this.connections = new Map();
-	}
+    constructor() {
+        this.connections = new Map();
+    }
 
-	async getConn(id, type, connSettings, forceNew = false) {
-		let conn = this.getConnection(id);
+    async getConn(id, type, connSettings, forceNew = false) {
+        let conn = this.getConnection(id);
 
-		if (conn && !forceNew) {
-			return conn;
-		} else return await this.setUpConnection(id, type, connSettings, false);
-	}
+        if (conn && !forceNew) {
+            return conn;
+        } else return await this.setUpConnection(id, type, connSettings, false);
+    }
 
-	getConnection(id) {
-		return this.connections.get(id);
-	}
+    getConnection(id) {
+        return this.connections.get(id);
+    }
 
-	addConnection(id, conn) {
-		this.connections.set(id, conn);
-	}
+    addConnection(id, conn) {
+        this.connections.set(id, conn);
+    }
 
-	async removeConnection(id, type) {
-		let conn = this.getConnection(id);
-		if (conn) {
-			try {
-				switch (type) {
-					case DATABASE.PostgreSQL:
-						await conn.end();
-						break;
-					case DATABASE.MySQL:
-						await conn.end();
-						break;
-					case DATABASE.SQLServer:
-						await conn.close();
-						break;
-					case DATABASE.MongoDB:
-						await conn.close();
-						break;
-					default:
-						break;
-				}
-			} catch (err) {}
-		}
+    async removeConnection(id, type) {
+        let conn = this.getConnection(id);
+        if (conn) {
+            try {
+                switch (type) {
+                    case DATABASE.PostgreSQL:
+                        await conn.end();
+                        break;
+                    case DATABASE.MySQL:
+                        await conn.end();
+                        break;
+                    case DATABASE.SQLServer:
+                        await conn.close();
+                        break;
+                    case DATABASE.MongoDB:
+                        await conn.close();
+                        break;
+                    default:
+                        break;
+                }
+            } catch (err) {}
+        }
 
-		this.connections.delete(id);
-	}
+        this.connections.delete(id);
+    }
 
-	async setUpConnection(id, type, connSettings) {
-		switch (type) {
-			case DATABASE.PostgreSQL:
-				try {
-					const client = new pg.Pool({
-						...helper.getAsObject(connSettings.options),
-						host: connSettings.host,
-						port: connSettings.port,
-						user: connSettings.username,
-						password: connSettings.password,
-						database: connSettings.database,
-						max: config.get("general.maxPoolSize"),
-					});
+    async setUpConnection(id, type, connSettings) {
+        switch (type) {
+            case DATABASE.PostgreSQL:
+                try {
+                    const client = new pg.Pool({
+                        ...helper.getAsObject(connSettings.options),
+                        host: connSettings.host,
+                        port: connSettings.port,
+                        user: connSettings.username,
+                        password: connSettings.password,
+                        database: connSettings.database,
+                        max: config.get("general.maxPoolSize"),
+                    });
 
-					await client.connect();
-					this.addConnection(id, client);
+                    await client.connect();
+                    this.addConnection(id, client);
 
-					return client;
-				} catch (err) {
-					throw new AgnostError(
-						t("Cannot connect to the PostgreSQL database. %s", err.message)
-					);
-				}
-			case DATABASE.MySQL:
-				try {
-					const connection = await mysql.createPool({
-						...helper.getAsObject(connSettings.options),
-						host: connSettings.host,
-						port: connSettings.port,
-						user: connSettings.username,
-						password: connSettings.password,
-						database: connSettings.database,
-						connectionLimit: config.get("general.maxPoolSize"),
-						multipleStatements: true,
-					});
+                    return client;
+                } catch (err) {
+                    throw new AgnostError(t("Cannot connect to the PostgreSQL database. %s", err.message));
+                }
+            case DATABASE.MySQL:
+                try {
+                    const connection = await mysql.createPool({
+                        ...helper.getAsObject(connSettings.options),
+                        host: connSettings.host,
+                        port: connSettings.port,
+                        user: connSettings.username,
+                        password: connSettings.password,
+                        database: connSettings.database,
+                        connectionLimit: config.get("general.maxPoolSize"),
+                        multipleStatements: true,
+                    });
 
-					this.addConnection(id, connection);
+                    this.addConnection(id, connection);
 
-					return connection;
-				} catch (err) {
-					throw new AgnostError(
-						t("Cannot connect to the MySQL database. %s", err.message)
-					);
-				}
-			case DATABASE.SQLServer:
-				try {
-					const connection = await mssql.connect({
-						...helper.getAsObject(connSettings.options),
-						server: connSettings.host,
-						port: connSettings.port,
-						user: connSettings.username,
-						password: connSettings.password,
-						database: connSettings.database,
-						encrypt: connSettings.encrypt ?? false,
-						pool: {
-							max: config.get("general.maxPoolSize"),
-						},
-					});
-					this.addConnection(id, connection);
+                    return connection;
+                } catch (err) {
+                    throw new AgnostError(t("Cannot connect to the MySQL database. %s", err.message));
+                }
+            case DATABASE.SQLServer:
+                try {
+                    const connection = await mssql.connect({
+                        requestTimeout: config.get("database.timeout"),
+                        ...helper.getAsObject(connSettings.options),
+                        server: connSettings.host,
+                        port: connSettings.port,
+                        user: connSettings.username,
+                        password: connSettings.password,
+                        database: connSettings.database,
+                        encrypt: connSettings.encrypt ?? false,
+                        pool: {
+                            max: config.get("general.maxPoolSize"),
+                        },
+                    });
+                    this.addConnection(id, connection);
 
-					return connection;
-				} catch (err) {
-					throw new AgnostError(
-						t("Cannot connect to the SQL Server database. %s", err.message)
-					);
-				}
-			case DATABASE.MongoDB:
-				try {
-					/* Example:
+                    return connection;
+                } catch (err) {
+                    throw new AgnostError(t("Cannot connect to the SQL Server database. %s", err.message));
+                }
+            case DATABASE.MongoDB:
+                try {
+                    /* Example:
 					{
 						"connFormat": "mongodb+srv",
 						"host": "cluster0-qxaly.mongodb.net",
@@ -130,52 +125,46 @@ class ConnectionManager {
 						"password": "G3oUiFhcKczQI6lJ"
 					}
 					 */
-					let client = null;
-					// Build query string part of the MongoDB connection string
-					connSettings.connOptions = helper.getQueryString(
-						connSettings.options
-					);
+                    let client = null;
+                    // Build query string part of the MongoDB connection string
+                    connSettings.connOptions = helper.getQueryString(connSettings.options);
 
-					if (connSettings.connFormat === "mongodb") {
-						let uri = `mongodb://${connSettings.host}:${connSettings.port}`;
-						if (connSettings.dbName) uri = `${uri}/${connSettings.dbName}`;
-						if (connSettings.connOptions)
-							uri = `${uri}?${connSettings.connOptions}`;
+                    if (connSettings.connFormat === "mongodb") {
+                        let uri = `mongodb://${connSettings.host}:${connSettings.port}`;
+                        if (connSettings.dbName) uri = `${uri}/${connSettings.dbName}`;
+                        if (connSettings.connOptions) uri = `${uri}?${connSettings.connOptions}`;
 
-						client = new mongo.MongoClient(uri, {
-							auth: {
-								username: connSettings.username,
-								password: connSettings.password,
-							},
-						});
-					} else {
-						let uri = `mongodb+srv://${connSettings.host}`;
-						if (connSettings.dbName) uri = `${uri}/${connSettings.dbName}`;
-						if (connSettings.connOptions)
-							uri = `${uri}?${connSettings.connOptions}`;
+                        client = new mongo.MongoClient(uri, {
+                            auth: {
+                                username: connSettings.username,
+                                password: connSettings.password,
+                            },
+                        });
+                    } else {
+                        let uri = `mongodb+srv://${connSettings.host}`;
+                        if (connSettings.dbName) uri = `${uri}/${connSettings.dbName}`;
+                        if (connSettings.connOptions) uri = `${uri}?${connSettings.connOptions}`;
 
-						client = new mongo.MongoClient(uri, {
-							auth: {
-								username: connSettings.username,
-								password: connSettings.password,
-							},
-						});
-					}
+                        client = new mongo.MongoClient(uri, {
+                            auth: {
+                                username: connSettings.username,
+                                password: connSettings.password,
+                            },
+                        });
+                    }
 
-					//Connect to the database of the application
-					await client.connect();
-					this.addConnection(id, client);
+                    //Connect to the database of the application
+                    await client.connect();
+                    this.addConnection(id, client);
 
-					return client;
-				} catch (err) {
-					throw new AgnostError(
-						t("Cannot connect to the MongoDB database. %s", err.message)
-					);
-				}
-			default:
-				throw new AgnostError(t("Unsupported database type"));
-		}
-	}
+                    return client;
+                } catch (err) {
+                    throw new AgnostError(t("Cannot connect to the MongoDB database. %s", err.message));
+                }
+            default:
+                throw new AgnostError(t("Unsupported database type"));
+        }
+    }
 }
 
 export default new ConnectionManager();
