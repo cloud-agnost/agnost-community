7697b0356bedc20a3987820ac17d555205fad94d
Özgür ÖZALP <ozgur@altogic.com>
2023-08-11 02:39:14 +0300

    data types updated

--- engine/worker/handlers/sql-database/data-types/DateTime.js
+++ engine/worker/handlers/sql-database/data-types/DateTime.js
@@ -1,3 +1,27 @@
 import Field from "./Field.js";
+import { DATABASE } from "../../../config/constants.js";
 
-export default class DateTime extends Field {}
+export default class DateTime extends Field {
+    defaultMap = {
+        [DATABASE.PostgreSQL]: " DEFAULT CURRENT_TIMESTAMP",
+        [DATABASE.MySQL]: " DEFAULT CURRENT_TIMESTAMP",
+        [DATABASE.SQLServer]: " DEFAULT CURRENT_TIMESTAMP",
+    };
+
+    toDefinitionQuery() {
+        let schema = this.createMap[this.getDatabaseType()];
+
+        if (!this.nullableFields.includes(this.getType())) {
+            schema += " {REQUIRED}";
+        }
+
+        if (this.getType() === "createdat") {
+            schema += this.defaultMap[this.getDatabaseType()];
+        }
+
+        return schema
+            .replace("{NAME}", this.getName())
+            .replace("{TYPE}", this.getDbType())
+            .replace("{REQUIRED}", this.isRequired() ? "NOT NULL" : "NULL");
+    }
+}
--- engine/worker/handlers/sql-database/data-types/Json.js
+++ engine/worker/handlers/sql-database/data-types/Json.js
@@ -1,3 +1,31 @@
 import Field from "./Field.js";
+import { DATABASE } from "../../../config/constants.js";
 
-export default class Json extends Field {}
+export default class Json extends Field {
+    createMap = {
+        [DATABASE.PostgreSQL]: "{NAME} {TYPE} {REQUIRED}",
+        [DATABASE.MySQL]: "`{NAME}` {TYPE} {REQUIRED}",
+        [DATABASE.SQLServer]: "{NAME} {TYPE}({MAX_LENGTH}) {REQUIRED}",
+    };
+
+    /**
+     * @description Gets the max length of the field
+     * @return {number}
+     */
+    getMaxLength() {
+        return config.get("database.jsonMaxLength") ?? 4000;
+    }
+
+    /**
+     * @description Generates the query for the field.
+     */
+    toDefinitionQuery() {
+        const schema = this.createMap[this.type];
+
+        return schema
+            .replace("{NAME}", this.getName())
+            .replace("{TYPE}", this.getDbType())
+            .replace("{MAX_LENGTH}", this.getMaxLength())
+            .replace("{REQUIRED}", this.isRequired() ? "NOT NULL" : "NULL");
+    }
+}
