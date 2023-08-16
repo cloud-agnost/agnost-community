6cf10222ef5efbedbc37405957685441263327c2
Enes Malik Ozer <enes@altogic.com>
2023-08-16 10:52:27 +0300

    Refactor import paths for Logs component in multiple files

--- studio/src/pages/queue/MessageQueueLogs.tsx
+++ studio/src/pages/queue/MessageQueueLogs.tsx
@@ -1,4 +1,4 @@
-import { VersionLogs } from '@/features/version/VersionLogs';
+import VersionLogs from '../../features/version/VersionLogs/VersionLogs';
 
 export default function MessageQueueLogs() {
 	return <VersionLogs type='queue' />;
