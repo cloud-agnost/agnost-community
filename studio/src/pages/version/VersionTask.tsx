642892dd59f25e02592324ade0c02adbd5a40104
Enes Malik Ozer <enes@altogic.com>
2023-08-31 09:44:45 +0300

    Refactor endpoint, queue, and task edit modals

--- studio/src/pages/version/VersionTask.tsx
+++ studio/src/pages/version/VersionTask.tsx
@@ -4,16 +4,17 @@ import { useState } from 'react';
 import { Table, Row } from '@tanstack/react-table';
 import CreateTask from '@/features/task/CreateTask';
 import EditTask from '@/features/task/EditTask';
+import useTaskStore from '@/store/task/taskStore';
 export default function VersionTask() {
 	const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
-	const [isEditTaskOpen, setIsEditTaskOpen] = useState(false);
 	const [selectedRows, setSelectedRows] = useState<Row<Task>[]>([]);
 	const [table, setTable] = useState<Table<Task>>();
 	const [page, setPage] = useState(0);
+	const { isEditTaskModalOpen, closeEditTaskModal } = useTaskStore();
 	return (
 		<>
 			<CreateTask open={isCreateModalOpen} onClose={() => setIsCreateModalOpen(false)} />
-			<EditTask open={isEditTaskOpen} onClose={() => setIsEditTaskOpen(false)} />
+			<EditTask open={isEditTaskModalOpen} onClose={closeEditTaskModal} />
 			<Outlet
 				context={{
 					isCreateModalOpen,
@@ -24,8 +25,6 @@ export default function VersionTask() {
 					setTable,
 					page,
 					setPage,
-					isEditTaskOpen,
-					setIsEditTaskOpen,
 				}}
 			/>
 		</>
