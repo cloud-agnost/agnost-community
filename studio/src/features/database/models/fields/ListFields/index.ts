97d3952a597e445b9bd6f1373e90ad76d3d2f416
Enes Malik Ozer <enes@altogic.com>
2023-09-01 13:24:01 +0300

    Delete FieldActions component

--- studio/src/features/database/models/fields/ListFields/index.ts
+++ studio/src/features/database/models/fields/ListFields/index.ts
@@ -2,5 +2,4 @@ export { default as ListFields } from './ListFields.tsx';
 export { default as CreateFieldButton } from './CreateFieldButton.tsx';
 export { default as EditOrCreateFieldDrawer } from './EditOrCreateFieldDrawer.tsx';
 export { default as FieldColumns } from './FieldColumns.tsx';
-export { default as FieldActions } from './FieldActions.tsx';
 export { default as SubFields } from './SubFields.tsx';
