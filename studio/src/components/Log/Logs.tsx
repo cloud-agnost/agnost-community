2dcc1da360c69f6d1811cd780e67d77e9452dc35
Enes Malik Ozer <enes@altogic.com>
2023-09-08 12:23:24 +0300

    Refactor Logs component to use p tags for log items

--- studio/src/components/Log/Logs.tsx
+++ studio/src/components/Log/Logs.tsx
@@ -4,10 +4,8 @@ interface LogsProps {
 
 export default function Logs({ logs }: LogsProps) {
 	return (
-		<div className='text-default text-sm font-mono bg-wrapper-background-light p-4 space-y-2'>
-			{logs?.map((log) => (
-				<div key={log}>{log}</div>
-			))}
+		<div className='text-default text-sm font-mono bg-base p-4 space-y-2 overflow-auto'>
+			{logs?.map((log) => <p key={log}>{log}</p>)}
 		</div>
 	);
 }
