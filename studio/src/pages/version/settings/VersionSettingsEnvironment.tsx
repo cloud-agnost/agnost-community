08e5046ff3ff39586979ad5d2c20267200bdc40a
Enes Malik Ozer <enes@altogic.com>
2023-09-19 16:37:22 +0300

    Add DeployButton import and move useTranslation import

--- studio/src/pages/version/settings/VersionSettingsEnvironment.tsx
+++ studio/src/pages/version/settings/VersionSettingsEnvironment.tsx
@@ -1,23 +1,10 @@
+import { DeployButton } from '@/features/version/DeployButton';
 import { SettingsContainer } from '@/features/version/SettingsContainer';
-import { useTranslation } from 'react-i18next';
 import { SettingsEnvironment } from '@/features/version/SettingsEnvironment';
 import useEnvironmentStore from '@/store/environment/environmentStore.ts';
 import { formatDate } from '@/utils';
 import { DateTime } from 'luxon';
-import { DeployButton } from '@/features/version/DeployButton';
-import { LoaderFunctionArgs } from 'react-router-dom';
-
-VersionSettingsEnvironment.loader = async ({ params }: LoaderFunctionArgs) => {
-	const { versionId, appId, orgId } = params;
-	const { getEnvironmentResources, environment } = useEnvironmentStore.getState();
-	await getEnvironmentResources({
-		orgId: orgId as string,
-		appId: appId as string,
-		versionId: versionId as string,
-		envId: environment?._id as string,
-	});
-	return { props: {} };
-};
+import { useTranslation } from 'react-i18next';
 
 export default function VersionSettingsEnvironment() {
 	const { t } = useTranslation();
