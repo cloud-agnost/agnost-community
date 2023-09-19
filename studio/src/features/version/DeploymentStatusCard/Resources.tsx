faa55a03f96c0152cc477c261f3c0c3b1bc91dc9
Enes Malik Ozer <enes@altogic.com>
2023-09-19 16:39:39 +0300

    Add new icons and update resource display

--- studio/src/features/version/DeploymentStatusCard/Resources.tsx
+++ studio/src/features/version/DeploymentStatusCard/Resources.tsx
@@ -1,33 +1,34 @@
-import { Database, MessageQueue, Storage } from 'components/icons';
+import { Database, MessageQueue, Storage, Cache, ApiKeys, Calendar } from 'components/icons';
 import { Badge } from 'components/Badge';
 import { BADGE_COLOR_MAP } from 'constants/constants.ts';
 import { useTranslation } from 'react-i18next';
-
-const resources = [
-	{
-		icon: <Database className='w-full h-full' />,
-		title: 'HR Database',
-		time: '03:39 PM',
-		status: 'Ok',
-	},
-	{
-		icon: <MessageQueue className='w-full h-full' />,
-		title: 'HR Database',
-		time: '03:39 PM',
-		status: 'Deploying',
-	},
-	{
-		icon: <Storage className='w-full h-full' />,
-		title: 'HR Database',
-		time: '03:39 PM',
-		status: 'Error',
-	},
-];
-
+import useEnvironmentStore from '@/store/environment/environmentStore';
+import { formatDate } from '@/utils';
 export default function Resources() {
 	const { t } = useTranslation();
+	const { resources } = useEnvironmentStore();
+
+	function getIcon(type: string) {
+		switch (type) {
+			case 'database':
+				return <Database className='w-5 h-5' />;
+			case 'storage':
+				return <Storage className='w-5 h-5' />;
+			case 'message-queue':
+				return <MessageQueue className='w-5 h-5' />;
+			case 'cache':
+				return <Cache className='w-5 h-5' />;
+			case 'engine':
+				return <ApiKeys className='w-5 h-5' />;
+			case 'task':
+				return <Calendar className='w-5 h-5' />;
+
+			default:
+				return null;
+		}
+	}
 	return (
-		<div className='w-full space-y-2 p-4'>
+		<div className='w-full space-y-2'>
 			<div className='text-subtle text-sm font-sfCompact leading-6 flex justify-between gap-4'>
 				<p>{t('version.resources')}</p>
 				<p>{t('version.status')}</p>
@@ -37,14 +38,18 @@ export default function Resources() {
 					<div key={index} className='py-[9px] flex justify-between gap-4'>
 						<div className='flex items-center gap-2'>
 							<span className='w-10 h-10 rounded-full bg-lighter flex items-center justify-center p-2'>
-								{resource.icon}
+								{getIcon(resource.type)}
 							</span>
 							<div className='flex flex-col'>
 								<p className='text-sm font-sfCompact font-normal leading-6 text-default'>
-									{resource.title}
+									{resource.name}
 								</p>
 								<time className='font-sfCompact text-[11px] leading-[21px] tracking-[0.22px] text-subtle font-normal'>
-									{resource.time}
+									{formatDate(resource.createdAt, {
+										month: 'short',
+										day: 'numeric',
+										year: 'numeric',
+									})}
 								</time>
 							</div>
 						</div>
