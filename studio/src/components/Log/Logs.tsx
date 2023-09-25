fb019227ca326c292c9505199399532ee1a832dc
Enes Malik Ozer <enes@altogic.com>
2023-09-26 01:09:19 +0300

    Refactor Log component for improved readability and styling

--- studio/src/components/Log/Logs.tsx
+++ studio/src/components/Log/Logs.tsx
@@ -20,11 +20,15 @@ export default function Logs({ logs, className }: LogsProps) {
 			{logs?.length ? (
 				logs?.map((log, index) => (
 					<div key={index} className='flex items-center gap-6  px-4 py-2'>
-						<p>{log.timestamp}</p>
+						<p className='self-start'>{log.timestamp}</p>
 						{log.type && (
-							<Badge variant={BADGE_COLOR_MAP[log.type.toUpperCase()]} text={log.type} />
+							<Badge
+								variant={BADGE_COLOR_MAP[log.type.toUpperCase()]}
+								text={log.type}
+								className='self-start'
+							/>
 						)}
-						<p className='whitespace-pre-wrap'>{log.message}</p>
+						<pre className='whitespace-pre-wrap'>{log.message}</pre>
 					</div>
 				))
 			) : (
