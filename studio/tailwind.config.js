b022c8227db56825722f046661e617da3a0c0b15
Enes Malik Ozer <enes@altogic.com>
2023-09-29 16:42:02 +0300

    Add animation classes for accordion down and up

--- studio/tailwind.config.js
+++ studio/tailwind.config.js
@@ -235,6 +235,10 @@ module.exports = {
 					from: { height: 'var(--radix-accordion-content-height)' },
 					to: { height: 0 },
 				},
+				animation: {
+					'accordion-down': 'accordion-down 0.2s ease-out',
+					'accordion-up': 'accordion-up 0.2s ease-out',
+				},
 			},
 		},
 	},
