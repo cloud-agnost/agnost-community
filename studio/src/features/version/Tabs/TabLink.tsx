b47ae1535baed2f92e00c87a9f7197b1b3f99099
Enes Malik Ozer <enes@altogic.com>
2023-10-09 22:33:24 +0300

    Refactor TabLink to handle tab navigation and current tab state

--- studio/src/features/version/Tabs/TabLink.tsx
+++ studio/src/features/version/Tabs/TabLink.tsx
@@ -1,8 +1,9 @@
 import { Button } from '@/components/Button';
 import { useTabNavigate } from '@/hooks';
+import useTabStore from '@/store/version/tabStore';
 import { TabTypes } from '@/types';
 import { cn } from '@/utils';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams, useNavigate } from 'react-router-dom';
 interface TabLinkProps {
 	className?: string;
 	name: string;
@@ -12,21 +13,35 @@ interface TabLinkProps {
 }
 
 export default function TabLink({ className, name, path, onClick, type, ...props }: TabLinkProps) {
-	const navigate = useTabNavigate();
+	const tabNavigate = useTabNavigate();
 	const { pathname } = useLocation();
+	const navigate = useNavigate();
+	const { getTabsByVersionId, setCurrentTab } = useTabStore();
+	const { versionId } = useParams<{ versionId: string }>();
+
+	function handleClickTabLink() {
+		onClick?.();
+		const tabs = getTabsByVersionId(versionId as string);
+		const tabPath = `${pathname}/${path}`;
+		const tab = tabs.find((tab) => tab.path === tabPath);
+
+		if (!tab) {
+			tabNavigate({
+				title: name,
+				path: tabPath,
+				isActive: true,
+				isDashboard: false,
+				type,
+			});
+		} else {
+			setCurrentTab(versionId as string, tab.id);
+			navigate(tabPath);
+		}
+	}
 	return (
 		<Button
 			variant='blank'
-			onClick={() => {
-				onClick?.();
-				navigate({
-					title: name,
-					path: `${pathname}/${path}`,
-					isActive: true,
-					isDashboard: false,
-					type,
-				});
-			}}
+			onClick={handleClickTabLink}
 			className={cn(className, 'link')}
 			{...props}
 		>
