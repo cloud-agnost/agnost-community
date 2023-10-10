7fe27538f7a87ce70f36bf1f4a2a3915208584fc
Koray Oksay <koray.oksay@gmail.com>
2023-10-10 19:09:10 +0300

    Update release flow

--- .github/scripts/update_helm_chart.py
+++ .github/scripts/update_helm_chart.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import os
 from ruamel.yaml import YAML
 
 yaml=YAML()
@@ -14,9 +15,10 @@ values_data = yaml.load(open(values_yaml).read())
 for app in applications:
     if '/' in app['application']:
         app_type, app_name = app['application'].split('/')
-        if app_name == 'core':
-            continue
         values_data[app_type][app_name]['tag'] = app['version']
+    else:
+        app_name = app['application']
+        values_data[app_name] = app['version']
 
 with open(values_yaml, 'w') as outfile:
     yaml.dump(values_data, outfile)
@@ -26,7 +28,10 @@ chart_data = yaml.load(open(chart_yaml).read())
 version = [int(n) for n in chart_data['version'].split('.')]
 version[2] += 1
 new_version = [str(n) for n in version]
+release_number = os.environ['RELEASE_NUMBER']
+
 chart_data['version'] = '.'.join(new_version)
+chart_data['appVersion'] = release_number
 
 with open(chart_yaml, 'w') as outfile:
     yaml.dump(chart_data, outfile)
