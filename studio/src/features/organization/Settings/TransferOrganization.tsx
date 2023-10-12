482e0635ce0318ad7b7480f280cb292904c9d15e
Enes Malik Ozer <enes@altogic.com>
2023-10-12 19:48:18 +0300

    Add TransferOwnership component to TransferOrganization

--- studio/src/features/organization/Settings/TransferOrganization.tsx
+++ studio/src/features/organization/Settings/TransferOrganization.tsx
@@ -1,71 +1,11 @@
-import { Alert, AlertDescription, AlertTitle } from '@/components/Alert';
-import { AutoComplete } from '@/components/AutoComplete';
-import { Button } from '@/components/Button';
+import { TransferOwnership } from '@/components/TransferOwnership';
 import { useToast } from '@/hooks';
-import useAuthorizeOrg from '@/hooks/useAuthorizeOrg';
 import useOrganizationStore from '@/store/organization/organizationStore';
-import { FormatOptionLabelProps, GroupedOption, OrganizationMember } from '@/types';
-import { APIError } from '@/types/type';
+import { APIError } from '@/types';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
-interface MemberSearch {
-	label: string;
-	value: OrganizationMember;
-}
-const loadOptions = async (inputValue: string) => {
-	const members: OrganizationMember[] = await useOrganizationStore
-		.getState?.()
-		.getOrganizationMembers({
-			search: inputValue,
-			excludeSelf: true,
-			organizationId: useOrganizationStore.getState?.().organization?._id as string,
-		});
-	return members.map((res) => ({
-		label: res.member.name,
-		value: res,
-	}));
-};
-const formatOptionLabel = ({ label, value }: FormatOptionLabelProps) => {
-	const name = label?.split(' ');
-	return (
-		<div className='flex items-center gap-2'>
-			{value.member.pictureUrl ? (
-				<img
-					src={value.member.pictureUrl}
-					alt={label}
-					className='rounded-full object-contain w-8 h-8 '
-				/>
-			) : (
-				name && (
-					<div
-						className='relative inline-flex items-center justify-center cursor-pointer overflow-hidden w-8 h-8 rounded-full'
-						style={{
-							backgroundColor: value.member.color,
-						}}
-					>
-						<span className='text-default text-xs'>
-							{name[0]?.charAt(0).toUpperCase()}
-							{name[1]?.charAt(0).toUpperCase()}
-						</span>
-					</div>
-				)
-			)}
-			<div className='flex flex-col'>
-				<span className='ml-2 text-default text-sm'>{label}</span>
-				<span className='ml-2 text-xs text-subtle'>{value.member.contactEmail}</span>
-			</div>
-		</div>
-	);
-};
-const formatGroupLabel = (data: GroupedOption<OrganizationMember>) => {
-	return (
-		<div className='flex items-center gap-2'>
-			<span className='text-default text-sm'>{data.label}</span>
-		</div>
-	);
-};
 export default function TransferOrganization() {
 	const [userId, setUserId] = useState<string>();
 	const [loading, setLoading] = useState(false);
@@ -73,12 +13,11 @@ export default function TransferOrganization() {
 	const { organization, transferOrganization } = useOrganizationStore();
 	const { notify } = useToast();
 	const navigate = useNavigate();
-	const canUpdate = useAuthorizeOrg('update');
 	const { t } = useTranslation();
 	function transferOrganizationHandle() {
 		setLoading(true);
 		transferOrganization({
-			organizationId: organization?._id as string,
+			organizationId: organization?._id,
 			userId: userId as string,
 			onSuccess: () => {
 				setLoading(false);
@@ -98,28 +37,12 @@ export default function TransferOrganization() {
 		});
 	}
 	return (
-		<div className='space-y-8'>
-			{error && (
-				<Alert variant='error'>
-					<AlertTitle>{error?.error}</AlertTitle>
-					<AlertDescription>{error?.details}</AlertDescription>
-				</Alert>
-			)}
-			<AutoComplete<MemberSearch>
-				loadOptions={loadOptions}
-				onChange={({ value }: MemberSearch) => setUserId(value.member._id)}
-				formatOptionLabel={formatOptionLabel}
-				formatGroupLabel={formatGroupLabel}
-			/>
-			<Button
-				size='lg'
-				className='text-end'
-				onClick={transferOrganizationHandle}
-				loading={loading}
-				disabled={!canUpdate || !userId}
-			>
-				{t('organization.transfer')}
-			</Button>
-		</div>
+		<TransferOwnership
+			transferOwnership={transferOrganizationHandle}
+			error={error as APIError}
+			loading={loading}
+			setUserId={setUserId}
+			userId={userId as string}
+		/>
 	);
 }
