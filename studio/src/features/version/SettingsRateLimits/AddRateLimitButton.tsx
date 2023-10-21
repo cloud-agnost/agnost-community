f258c9c5aedce9d858e856d8e8b05a6dbf51a9d5
Enes Malik Ozer <enes@altogic.com>
2023-10-21 17:15:26 +0300

    Rename AddMiddlewareButton to AddRateLimitButton

--- studio/src/features/version/SettingsRateLimits/AddRateLimitButton.tsx
+++ studio/src/features/version/SettingsRateLimits/AddRateLimitButton.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
 import { EditOrAddEndpointRateLimiterDrawer } from '@/features/version/SettingsGeneral';
 
-export default function AddMiddlewareButton() {
+export default function AddRateLimitButton() {
 	const { t } = useTranslation();
 	const [open, setOpen] = useState(false);
 
