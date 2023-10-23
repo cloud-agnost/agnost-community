95a77f377c8ff98a5bed6176b4d2ab67b4e59e17
Enes Malik Ozer <enes@altogic.com>
2023-10-23 16:29:16 +0300

    refactor database folder structure

--- studio/src/pages/version/models/fields/Fields.tsx
+++ studio/src/pages/version/models/fields/Fields.tsx
@@ -1,16 +1,32 @@
+import {
+	CreateFieldButton,
+	EditOrCreateFieldDrawer,
+	FieldColumns,
+} from '@/features/database/models/fields/ListFields';
+import useAuthorizeVersion from '@/hooks/useAuthorizeVersion.tsx';
+import { VersionTabLayout } from '@/layouts/VersionLayout';
+import useDatabaseStore from '@/store/database/databaseStore.ts';
 import useModelStore from '@/store/database/modelStore.ts';
-import { ListFields, EditOrCreateFieldDrawer } from '@/features/database/models/fields/ListFields';
-import { useParams } from 'react-router-dom';
-import { Model } from '@/types';
+import { Database, Field, Model } from '@/types';
+import { Row, Table } from '@tanstack/react-table';
+import { BreadCrumb, BreadCrumbItem } from 'components/BreadCrumb';
+import { DataTable } from 'components/DataTable';
 import { useEffect, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 export default function Fields() {
 	const { modelId, versionId, dbId, appId, orgId } = useParams() as Record<string, string>;
 	const [parentModelIid, setParentModelIid] = useState<string>();
-	const getSpecificModelOfDatabase = useModelStore((state) => state.getSpecificModelOfDatabase);
-	const { isOpenEditFieldDialog, setIsOpenEditFieldDialog } = useModelStore();
-
-	const models = useModelStore((state) => state.models);
+	const { isOpenEditFieldDialog, setIsOpenEditFieldDialog, models, getSpecificModelOfDatabase } =
+		useModelStore();
+	const [selectedRows, setSelectedRows] = useState<Row<Field>[]>();
+	const { t } = useTranslation();
+	const [table, setTable] = useState<Table<Field>>();
+	const { databases } = useDatabaseStore();
+	const { deleteMultipleField } = useModelStore();
+	const canMultiDelete = useAuthorizeVersion('model.delete');
+	const [searchParams] = useSearchParams();
 
 	const model = useMemo(
 		() => models.find((model) => model._id === modelId) as Model,
@@ -37,9 +53,96 @@ export default function Fields() {
 		setParentModelIid(model.parentiid);
 	}
 
+	const filteredFields = useMemo(() => {
+		const search = searchParams.get('q') ?? '';
+		if (!search) return model.fields;
+
+		return model.fields
+			.filter((f) => {
+				return f.name.toLowerCase().includes(search.toLowerCase());
+			})
+			.sort((a, b) => b.order - a.order);
+	}, [searchParams.get('q'), model]);
+
+	useEffect(() => {
+		setSelectedRows((selectedRows) => {
+			return selectedRows?.filter((row) => row.original.creator !== 'system');
+		});
+		if (table) {
+			table?.setRowSelection((updater) => {
+				return updater;
+			});
+		}
+	}, []);
+
+	const database = useMemo(() => {
+		return databases.find((database) => database._id === dbId) as Database;
+	}, [databases, dbId]);
+
+	async function deleteHandler() {
+		await deleteMultipleField({
+			dbId: model.dbId,
+			modelId: model._id,
+			appId: database.appId,
+			orgId: database.orgId,
+			versionId: database.versionId,
+			fieldIds: selectedRows?.map((row) => row.original._id) as string[],
+		});
+		setSelectedRows(undefined);
+		table?.resetRowSelection?.();
+	}
+
+	const databasesUrl = `/organization/${database?.orgId}/apps/${database?.appId}/version/${database?.versionId}/database`;
+	const databaseUrl = `${databasesUrl}/${model.dbId}/models`;
+	const goParentModelUrl = `${databaseUrl}/${parentModel?._id}/fields`;
+
+	const breadcrumbItems: BreadCrumbItem[] = [
+		{
+			name: t('database.page_title').toString(),
+			url: databasesUrl,
+		},
+		{
+			name: database?.name,
+			url: databaseUrl,
+		},
+		{
+			name: parentModel?.name,
+			url: goParentModelUrl,
+		},
+		{
+			name: model?.name,
+		},
+	];
 	return (
 		<>
-			{model && <ListFields key={model._id} parentModel={parentModel} model={model} />}
+			{model && (
+				<VersionTabLayout<Field>
+					breadCrumb={
+						<BreadCrumb
+							goBackLink={parentModel ? goParentModelUrl : databaseUrl}
+							items={breadcrumbItems}
+						/>
+					}
+					isEmpty={!filteredFields.length}
+					title={t('database.fields.title')}
+					type='field'
+					handlerButton={<CreateFieldButton />}
+					emptyStateTitle={t('database.fields.no_fields')}
+					table={table}
+					selectedRowLength={selectedRows?.length}
+					onSearch={() => {}}
+					disabled={!canMultiDelete}
+					onMultipleDelete={deleteHandler}
+				>
+					<DataTable<Field>
+						setTable={setTable}
+						columns={FieldColumns}
+						data={filteredFields}
+						noDataMessage={<p className='text-xl'>{t('database.fields.no_fields')}</p>}
+						setSelectedRows={setSelectedRows}
+					/>
+				</VersionTabLayout>
+			)}
 			<EditOrCreateFieldDrawer
 				key={isOpenEditFieldDialog.toString()}
 				open={isOpenEditFieldDialog}
