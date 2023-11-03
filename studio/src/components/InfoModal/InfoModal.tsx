2ab88a00a4499a5f10f424075daafa738e696471
Enes Malik Ozer <enes@altogic.com>
2023-11-03 15:42:32 +0300

    Refactor InfoModal component to use Dialog component

--- studio/src/components/InfoModal/InfoModal.tsx
+++ studio/src/components/InfoModal/InfoModal.tsx
@@ -1,52 +1,57 @@
-import { Modal, ModalProps } from 'components/Modal';
-import { Button } from 'components/Button';
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogDescription,
+} from '../Dialog';
+
+import { Button, buttonVariants } from 'components/Button';
 import { ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 import './infoModal.scss';
+import { cn } from '@/utils';
+import { Warning } from '../icons';
 
-interface InfoModalProps extends ModalProps {
+interface InfoModalProps {
 	title: string;
 	description: string;
-	icon?: ReactNode;
-	action?: ReactNode;
-	closable?: boolean;
-	closeButtonText?: string;
+	isOpen: boolean;
+	closeModal: () => void;
+	className?: string;
+	onConfirm: () => void;
 }
 export default function InfoModal({
 	closeModal,
-	closeOnOverlayClick,
+	className,
 	isOpen,
 	description,
 	title,
-	icon,
-	action,
-	closable,
-	className,
-	closeButtonText,
+	onConfirm,
 }: InfoModalProps) {
 	const { t } = useTranslation();
 	return (
-		<Modal
-			closeModal={closeModal}
-			className={className}
-			closeOnOverlayClick={closeOnOverlayClick}
-			isOpen={isOpen}
-		>
-			<div className='info-modal'>
-				{icon}
-				<div className='info-modal-text'>
-					<h2 className='info-modal-text-title'>{title}</h2>
-					<p className='info-modal-text-desc'>{description}</p>
-				</div>
-				<div className='info-modal-actions'>
-					{closable && (
-						<Button variant='text' size='lg'>
-							{closeButtonText ?? t('general.cancel')}
-						</Button>
-					)}
-					{action}
-				</div>
-			</div>
-		</Modal>
+		<Dialog open={isOpen} onOpenChange={closeModal}>
+			<DialogContent className={cn('space-y-4', className)}>
+				<DialogHeader className='flex flex-col items-center gap-2'>
+					<Warning className='text-icon-danger w-20 h-20' />
+					<DialogTitle>{title}</DialogTitle>
+					<DialogDescription className='text-center'>{description}</DialogDescription>
+				</DialogHeader>
+				<DialogFooter className='flex justify-center'>
+					<DialogClose
+						className={cn(buttonVariants({ variant: 'text', size: 'lg' }))}
+						onClick={closeModal}
+					>
+						{t('general.cancel')}
+					</DialogClose>
+					<Button onClick={onConfirm} size='lg'>
+						{t('general.ok')}
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
 	);
 }
