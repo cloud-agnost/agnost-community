be83241c58b8c66f0e75563561445c9400342149
Enes Malik Ozer <enes@altogic.com>
2023-11-03 15:45:52 +0300

    Update MODULE_PAGE_SIZE constant to 25

--- studio/src/pages/middleware/Middleware.tsx
+++ studio/src/pages/middleware/Middleware.tsx
@@ -1,4 +1,4 @@
-import { PAGE_SIZE } from '@/constants';
+import { MODULE_PAGE_SIZE, PAGE_SIZE } from '@/constants';
 import { AddMiddlewareDrawer } from '@/features/version/Middlewares';
 import MiddlewaresColumns from '@/features/version/Middlewares/MiddlewaresColumns.tsx';
 import useAuthorizeVersion from '@/hooks/useAuthorizeVersion.tsx';
@@ -12,17 +12,24 @@ import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { useParams, useSearchParams } from 'react-router-dom';
-import { useToast } from '@/hooks';
+import { usePage, useToast } from '@/hooks';
+import localforage from 'localforage';
 export default function MainMiddleware() {
 	const { notify } = useToast();
 	const [selectedRows, setSelectedRows] = useState<Row<Middleware>[]>();
-	const { getMiddlewaresOfAppVersion, deleteMultipleMiddlewares, middlewares, lastFetchedCount } =
-		useMiddlewareStore();
+	const {
+		getMiddlewaresOfAppVersion,
+		deleteMultipleMiddlewares,
+		middlewares,
+		lastFetchedCount,
+		lastFetchedPage,
+	} = useMiddlewareStore();
+	const { page, incrementPage } = usePage();
 	const [loading, setLoading] = useState(false);
-	const [page, setPage] = useState(0);
 	const [table, setTable] = useState<Table<Middleware>>();
 	const { orgId, appId, versionId } = useParams();
 	const canCreate = useAuthorizeVersion('middleware.create');
+
 	const [searchParams] = useSearchParams();
 	const { t } = useTranslation();
 	const [open, setOpen] = useState(false);
@@ -35,11 +42,7 @@ export default function MainMiddleware() {
 			appId: appId as string,
 			middlewareIds: rows?.map((row) => row._id) as string[],
 			onSuccess: () => {
-				notify({
-					title: t('general.success'),
-					description: t('version.middleware.delete.success'),
-					type: 'success',
-				});
+				table?.toggleAllRowsSelected(false);
 			},
 			onError: (error) => {
 				notify({
@@ -52,47 +55,50 @@ export default function MainMiddleware() {
 	}
 
 	useEffect(() => {
-		setLoading(true);
-		getMiddlewaresOfAppVersion({
-			orgId: orgId as string,
-			versionId: versionId as string,
-			appId: appId as string,
-			size: PAGE_SIZE,
-			page,
-			search: searchParams.get('q') as string,
-		});
-		setLoading(false);
+		if (page === 0 || page > lastFetchedPage) {
+			setLoading(true);
+			getMiddlewaresOfAppVersion({
+				orgId: orgId as string,
+				versionId: versionId as string,
+				appId: appId as string,
+				page,
+				size: MODULE_PAGE_SIZE,
+				search: searchParams.get('q') as string,
+			});
+			setLoading(false);
+		}
 	}, [page, searchParams.get('q')]);
 	return (
-		<VersionTabLayout<Middleware>
-			className='p-0'
-			type='middleware'
-			title={t('version.settings.middlewares')}
-			emptyStateTitle={t('version.middleware.no_middleware_found')}
-			createButtonTitle={t('version.middleware.add_middleware')}
-			isEmpty={!middlewares.length}
-			openCreateModal={() => setOpen(true)}
-			onMultipleDelete={deleteMultipleMiddlewaresHandler}
-			onSearch={() => setPage(0)}
-			table={table}
-			selectedRowLength={selectedRows?.length}
-			disabled={!canCreate}
-		>
-			<InfiniteScroll
-				scrollableTarget='version-layout'
-				dataLength={middlewares.length}
-				next={() => setPage(page + 1)}
-				hasMore={lastFetchedCount >= PAGE_SIZE}
-				loader={loading && <TableLoading />}
+		<>
+			<VersionTabLayout<Middleware>
+				className='p-0'
+				type='middleware'
+				title={t('version.settings.middlewares')}
+				emptyStateTitle={t('version.middleware.no_middleware_found')}
+				createButtonTitle={t('version.middleware.add_middleware')}
+				isEmpty={!middlewares.length}
+				openCreateModal={() => setOpen(true)}
+				onMultipleDelete={deleteMultipleMiddlewaresHandler}
+				table={table}
+				selectedRowLength={selectedRows?.length}
+				disabled={!canCreate}
 			>
-				<DataTable<Middleware>
-					columns={MiddlewaresColumns}
-					data={middlewares}
-					setTable={setTable}
-					setSelectedRows={setSelectedRows}
-				/>
-			</InfiniteScroll>
+				<InfiniteScroll
+					scrollableTarget='version-layout'
+					dataLength={middlewares.length}
+					next={incrementPage}
+					hasMore={lastFetchedCount >= PAGE_SIZE}
+					loader={loading && <TableLoading />}
+				>
+					<DataTable<Middleware>
+						columns={MiddlewaresColumns}
+						data={middlewares}
+						setTable={setTable}
+						setSelectedRows={setSelectedRows}
+					/>
+				</InfiniteScroll>
+			</VersionTabLayout>
 			<AddMiddlewareDrawer open={open} onOpenChange={setOpen} />
-		</VersionTabLayout>
+		</>
 	);
 }
