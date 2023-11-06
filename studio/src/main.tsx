3cb3668bfa1692c1ef2438a9ae0efeef520da89c
Enes Malik Ozer <enes@altogic.com>
2023-11-06 20:13:56 +0300

    Refactor render method in main.tsx

--- studio/src/main.tsx
+++ studio/src/main.tsx
@@ -2,8 +2,4 @@ import App from '@/App.tsx';
 import '@/index.scss';
 import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<>
-		<App />
-	</>,
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
