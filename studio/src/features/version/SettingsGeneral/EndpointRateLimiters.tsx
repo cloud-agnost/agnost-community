a5d2708d86b27c5eceaac47b0be2024411c05641
Enes Malik Ozer <enes@altogic.com>
2023-11-25 04:34:02 +0300

    Update general settings structure

--- studio/src/features/version/SettingsGeneral/EndpointRateLimiters.tsx
+++ studio/src/features/version/SettingsGeneral/EndpointRateLimiters.tsx
@@ -1,22 +1,21 @@
+import { SettingsFormItem } from '@/components/SettingsFormItem';
 import { SortableRateLimits } from '@/features/version/SettingsGeneral';
-import { useToast } from '@/hooks';
+import { useUpdateVersion } from '@/hooks';
 import useSettingsStore from '@/store/version/settingsStore';
 import useVersionStore from '@/store/version/versionStore.ts';
 import { RateLimit } from '@/types';
 import { reorder } from '@/utils';
-import { useState } from 'react';
 import { DropResult } from 'react-beautiful-dnd';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 export default function EndpointRateLimiters() {
 	const { t } = useTranslation();
-	const { notify } = useToast();
-	const [deleting, setDeleting] = useState(false);
+
+	const { updateVersion, isPending } = useUpdateVersion();
 	const defaultEndpointLimits = useVersionStore((state) => state.version?.defaultEndpointLimits);
 	const defaultRateLimiters = useVersionStore(
 		(state) => state.version?.defaultEndpointLimits ?? [],
 	);
-	const updateVersionProperties = useVersionStore((state) => state.updateVersionProperties);
 	const rateLimits = useVersionStore((state) => state.version?.limits);
 
 	const rateLimitsNotInDefault = rateLimits?.filter(
@@ -33,82 +32,39 @@ export default function EndpointRateLimiters() {
 		if (!result.destination || !defaultRateLimiters || !versionId || !appId || !orgId) return;
 		const ordered = reorder(defaultRateLimiters, result.source.index, result.destination.index);
 		orderLimits(ordered);
-		await updateVersionProperties({
-			orgId,
-			versionId,
-			appId,
+		updateVersion({
 			defaultEndpointLimits: ordered,
-			onError: (error) => {
-				notify({
-					type: 'error',
-					title: t('general.error'),
-					description: error.details,
-				});
-			},
 		});
 	}
 
 	async function addToDefault(limiter: RateLimit) {
 		if (!defaultRateLimiters || !versionId || !appId || !orgId) return;
-		updateVersionProperties({
-			orgId,
-			versionId,
-			appId,
-			defaultEndpointLimits: [...(defaultRateLimiters ?? []), limiter.iid],
-			onSuccess: () => {
-				notify({
-					type: 'success',
-					title: t('general.success'),
-					description: t('version.limiter_added_to_default'),
-				});
-			},
-			onError: (error) => {
-				notify({
-					type: 'error',
-					title: t('general.error'),
-					description: error.details,
-				});
-			},
-		});
+		updateVersion({ defaultEndpointLimits: [...(defaultRateLimiters ?? []), limiter.iid] });
 	}
 
 	async function deleteHandler(limitId?: string) {
-		if (!versionId || !appId || !orgId || deleting || !limitId) return;
-
-		setDeleting(true);
-		updateVersionProperties({
-			orgId,
-			versionId,
-			appId,
+		if (!versionId || !appId || !orgId || !limitId) return;
+		updateVersion({
 			defaultEndpointLimits: defaultEndpointLimits?.filter((item) => item !== limitId),
-			onSuccess: () => {
-				notify({
-					type: 'success',
-					title: t('general.success'),
-					description: t('version.limiter_added_to_default'),
-				});
-				setDeleting(false);
-			},
-			onError: (error) => {
-				notify({
-					type: 'error',
-					title: t('general.error'),
-					description: error.details,
-				});
-				setDeleting(false);
-			},
 		});
 	}
 
 	return (
-		<SortableRateLimits
-			onDragEnd={onDragEnd}
-			options={rateLimitsNotInDefault}
-			onSelect={addToDefault}
-			selectedLimits={defaultRateLimiters}
-			onDeleteItem={(limitId: string) => deleteHandler(limitId)}
-			loading={deleting}
-			hasToAddAsDefault='endpoint'
-		/>
+		<SettingsFormItem
+			className='space-y-0 py-0 pt-6'
+			contentClassName='pt-6'
+			title={t('version.settings.endpoint_rate_limit')}
+			description={t('version.settings.endpoint_rate_limit_desc')}
+		>
+			<SortableRateLimits
+				onDragEnd={onDragEnd}
+				options={rateLimitsNotInDefault}
+				onSelect={addToDefault}
+				selectedLimits={defaultRateLimiters}
+				onDeleteItem={(limitId: string) => deleteHandler(limitId)}
+				loading={isPending}
+				hasToAddAsDefault='endpoint'
+			/>
+		</SettingsFormItem>
 	);
 }
--- studio/src/features/version/SettingsGeneral/index.ts
+++ studio/src/features/version/SettingsGeneral/index.ts
@@ -1,5 +1,7 @@
-export { default as SettingsGeneral } from './SettingsGeneral.tsx';
-export { default as EndpointRateLimiters } from './EndpointRateLimiters.tsx';
-export { default as EditOrAddEndpointRateLimiterDrawer } from './EditOrAddEndpointRateLimiterDrawer.tsx';
-export { default as SortableRateLimits } from './SortableRateLimits.tsx';
 export { default as AddRateLimiterDropdown } from './AddRateLimiterDropdown.tsx';
+export { default as BaseUrl } from './BaseUrl.tsx';
+export { default as EditOrAddEndpointRateLimiterDrawer } from './EditOrAddEndpointRateLimiterDrawer.tsx';
+export { default as EndpointRateLimiters } from './EndpointRateLimiters.tsx';
+export { default as SortableRateLimits } from './SortableRateLimits.tsx';
+export { default as UpdateVersionName } from './UpdateVersionName.tsx';
+export { default as UpdateVersionVisibility } from './UpdateVersionVisibility.tsx';
