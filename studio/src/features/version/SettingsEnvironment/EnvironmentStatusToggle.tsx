19d4ed81b994253f45b8cdf81c1d9fb4019c79ba
Enes Malik Ozer <enes@altogic.com>
2023-11-28 16:49:12 +0300

    Refactor environment status toggle component and add suspend button

--- studio/src/features/version/SettingsEnvironment/EnvironmentStatusToggle.tsx
+++ studio/src/features/version/SettingsEnvironment/EnvironmentStatusToggle.tsx
@@ -1,32 +1,11 @@
-import { Button } from '@/components/Button';
 import { SettingsFormItem } from '@/components/SettingsFormItem';
-import { useAuthorizeVersion } from '@/hooks';
 import useEnvironmentStore from '@/store/environment/environmentStore';
-import { cn } from '@/utils';
 import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
+import { SuspendButton } from '../SuspendButton';
 
 export default function EnvironmentStatusToggle() {
 	const { t } = useTranslation();
 	const environment = useEnvironmentStore((state) => state.environment);
-	const canEdit = useAuthorizeVersion('env.update');
-	const { suspendEnvironment, activateEnvironment } = useEnvironmentStore();
-	const { orgId, versionId, appId } = useParams<{
-		versionId: string;
-		appId: string;
-		orgId: string;
-	}>();
-
-	async function suspendOrActive() {
-		if (!versionId || !appId || !orgId || !environment?._id) return;
-		const func = environment?.suspended ? activateEnvironment : suspendEnvironment;
-		func({
-			envId: environment._id,
-			orgId,
-			appId,
-			versionId,
-		});
-	}
 
 	return (
 		<SettingsFormItem
@@ -41,14 +20,7 @@ export default function EnvironmentStatusToggle() {
 					: t('version.suspend_services_desc')
 			}
 		>
-			<Button
-				disabled={!canEdit}
-				className={cn(!environment?.suspended && '!text-elements-red')}
-				variant={environment?.suspended ? 'primary' : 'outline'}
-				onClick={suspendOrActive}
-			>
-				{environment?.suspended ? t('version.reactivate') : t('version.suspend')}
-			</Button>
+			<SuspendButton />
 		</SettingsFormItem>
 	);
 }
