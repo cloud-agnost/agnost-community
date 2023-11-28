684165d1b6a0a28487e24d664b27ef1ed3c9c723
Enes Malik Ozer <enes@altogic.com>
2023-11-28 14:57:49 +0300

    Refactor CopyInput component and add useUpdateEffect hook

--- studio/src/components/CopyInput/CopyInput.tsx
+++ studio/src/components/CopyInput/CopyInput.tsx
@@ -5,10 +5,15 @@ import * as React from 'react';
 import { useState } from 'react';
 import { Button } from '../Button';
 import './copyInput.scss';
+import { useUpdateEffect } from '@/hooks';
 
 const CopyInput = React.forwardRef<HTMLInputElement, React.ComponentPropsWithoutRef<'input'>>(
 	({ className, value, readOnly, placeholder, ...props }, ref) => {
 		const [inputValue, setInputValue] = useState<string>(value as string);
+
+		useUpdateEffect(() => {
+			setInputValue(value as string);
+		}, [value]);
 
 		return (
 			<div className={cn('copy-input-wrapper', className)} {...props}>
