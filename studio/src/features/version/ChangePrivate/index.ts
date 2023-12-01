6a088cfff13270c38e5d38a5f7f5a2b98f940930
Enes Malik Ozer <enes@altogic.com>
2023-12-02 01:51:38 +0300

    Delete unused components and styles

--- studio/src/features/version/ChangePrivate/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ChangePrivate } from './ChangePrivate.tsx';
--- studio/src/features/version/ChangeReadOnly/ChangeReadOnly.tsx
+++
@@ -1,7 +0,0 @@
-import useVersionStore from '@/store/version/versionStore.ts';
-import { Switch } from 'components/Switch';
-
-export default function ChangeReadOnly() {
-	const { version } = useVersionStore();
-	return <Switch defaultChecked={version?.readOnly} />;
-}
