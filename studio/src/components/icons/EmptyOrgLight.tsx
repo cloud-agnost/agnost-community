5ea68cea34b71d58a24e3ca07ec3977695de45b6
Enes Malik Ozer <enes@altogic.com>
2023-12-12 09:37:58 +0300

    Add EmptyOrg and EmptyOrgLight SVG icons

---
+++ studio/src/components/icons/EmptyOrgLight.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+const SvgEmptyOrgLight = (props: SVGProps<SVGSVGElement>) => (
+	<svg
+		width='1em'
+		height='1em'
+		viewBox='0 0 173 172'
+		fill='none'
+		xmlns='http://www.w3.org/2000/svg'
+		{...props}
+	>
+		<path
+			d='M82.17 142.34c32.679 0 59.17-26.491 59.17-59.17S114.849 24 82.17 24 23 50.491 23 83.17s26.491 59.17 59.17 59.17Z'
+			fill='#F4F4F4'
+			stroke='#949AB3'
+			strokeWidth={2}
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		/>
+		<path
+			d='M82.17 87.08c13.084 0 23.69-10.606 23.69-23.69 0-13.084-10.606-23.69-23.69-23.69-13.083 0-23.69 10.606-23.69 23.69 0 13.084 10.607 23.69 23.69 23.69Z'
+			fill='url(#EmptyOrgLight_svg__a)'
+			stroke='#949AB3'
+			strokeWidth={2}
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		/>
+		<path
+			d='M82.17 92.56c-17.87 0-32.35 14.48-32.35 32.35v7.8c9.3 6.09 20.41 9.64 32.35 9.64a58.85 58.85 0 0 0 32.35-9.64v-7.8c0-17.87-14.48-32.35-32.35-32.35Z'
+			fill='url(#EmptyOrgLight_svg__b)'
+			stroke='#949AB3'
+			strokeWidth={2}
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		/>
+		<path
+			d='M82.17 142.34c32.679 0 59.17-26.491 59.17-59.17S114.849 24 82.17 24 23 50.491 23 83.17s26.491 59.17 59.17 59.17Z'
+			stroke='#6B6D6E'
+			strokeWidth={2}
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		/>
+		<path
+			d='M127.75 148.4c12.614 0 22.84-10.226 22.84-22.84 0-12.614-10.226-22.84-22.84-22.84-12.614 0-22.84 10.226-22.84 22.84 0 12.614 10.226 22.84 22.84 22.84Z'
+			fill='#F4F4F4'
+			stroke='#6B6D6E'
+			strokeWidth={2}
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		/>
+		<path
+			d='M127.751 139.68c7.798 0 14.12-6.322 14.12-14.12 0-7.798-6.322-14.12-14.12-14.12-7.798 0-14.12 6.322-14.12 14.12 0 7.798 6.322 14.12 14.12 14.12ZM117.791 115.55l19.93 19.92'
+			stroke='#6B6D6E'
+			strokeWidth={2}
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		/>
+		<defs>
+			<linearGradient
+				id='EmptyOrgLight_svg__a'
+				x1={80.3}
+				y1={1.07}
+				x2={84.281}
+				y2={134.01}
+				gradientUnits='userSpaceOnUse'
+			>
+				<stop stopColor='#E0E3EB' />
+				<stop offset={0.18} stopColor='#D1D7E4' />
+				<stop offset={0.35} stopColor='#B4BFD6' />
+				<stop offset={0.5} stopColor='#9BA7C2' />
+				<stop offset={0.62} stopColor='#8994B4' />
+				<stop offset={0.68} stopColor='#8390AF' />
+			</linearGradient>
+			<linearGradient
+				id='EmptyOrgLight_svg__b'
+				x1={81.28}
+				y1={42.92}
+				x2={82.55}
+				y2={165.36}
+				gradientUnits='userSpaceOnUse'
+			>
+				<stop stopColor='#E0E3EB' />
+				<stop offset={0.08} stopColor='#C6D0E1' />
+				<stop offset={0.28} stopColor='#A2B4D0' />
+				<stop offset={0.46} stopColor='#7D98BF' />
+				<stop offset={0.62} stopColor='#5E78B3' />
+				<stop offset={0.75} stopColor='#475DAA' />
+				<stop offset={0.83} stopColor='#3D54A8' />
+			</linearGradient>
+		</defs>
+	</svg>
+);
+export default SvgEmptyOrgLight;
