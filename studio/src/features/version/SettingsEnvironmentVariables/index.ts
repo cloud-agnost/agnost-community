4abb53f9ce1c6f3111a4fd55a35af5d0878ce852
Enes Malik Ozer <enes@altogic.com>
2023-12-12 09:42:19 +0300

    Refactor environment variable settings components

--- studio/src/features/version/SettingsEnvironmentVariables/index.ts
+++ studio/src/features/version/SettingsEnvironmentVariables/index.ts
@@ -1,4 +1,4 @@
 export { default as AddVariableButton } from './AddVariableButton.tsx';
-export { default as EditOrAddVariableDrawer } from './EditOrAddVariableDrawer.tsx';
 export { default as VariableActions } from './VariableActions.tsx';
+export { default as EditEnvVariable } from './EditEnvVariable.tsx';
 export { default as VariableColumns } from './VariableColumns.tsx';
