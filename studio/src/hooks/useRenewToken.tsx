70ec289aafd9dd15abe0f67429ddf335c0360f11
Enes Malik Ozer <enes@altogic.com>
2023-12-14 09:53:36 +0300

    Remove unused variable in useRenewToken hook

--- studio/src/hooks/useRenewToken.tsx
+++ studio/src/hooks/useRenewToken.tsx
@@ -3,12 +3,12 @@ import useAuthStore from '@/store/auth/authStore.ts';
 
 let interval: number | null = null;
 export default function useRenewToken(mins = 2) {
-	const { user, renewAccessToken } = useAuthStore();
+	const { renewAccessToken } = useAuthStore();
 
 	useEffect(() => {
 		interval = window.setInterval(() => renewAccessToken(), mins * 60 * 1000);
 		return () => {
 			if (interval) window.clearInterval(interval);
 		};
-	}, [user]);
+	}, []);
 }
