2099572352bbdd00e64c5122534b9880c2876589
Enes Malik Ozer <enes@altogic.com>
2023-12-18 10:22:30 +0300

    Refactor useSearch hook to include sorting functionality

--- studio/src/hooks/useSearch.tsx
+++ studio/src/hooks/useSearch.tsx
@@ -1,14 +1,22 @@
+import { sortByField } from '@/utils';
 import { useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
+
 export default function useSearch(data: any[]) {
 	const [searchParams] = useSearchParams();
+	const field = searchParams.get('f') || '';
+	const dir = (searchParams.get('d') || '') as 'asc' | 'desc';
 	const filteredData = useMemo(() => {
 		if (searchParams.get('q')) {
 			const query = new RegExp(searchParams.get('q') as string, 'i');
-			return data.filter((val) => RegExp(query).exec(val.name));
+			return sortByField(
+				data.filter((item) => query.test(item.name)),
+				field,
+				dir,
+			);
 		}
-		return data;
-	}, [searchParams.get('q'), data]);
+		return sortByField(data, field, dir);
+	}, [searchParams.get('q'), searchParams.get('f'), searchParams.get('d'), data]);
 
 	return filteredData;
 }
