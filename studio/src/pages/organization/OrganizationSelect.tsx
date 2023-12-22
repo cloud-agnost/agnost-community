cf96ce14d513f1aa0090c0f280b435984cdae950
Enes Malik Ozer <enes@altogic.com>
2023-12-22 17:46:52 +0300

    Add loading spinner and refactor OrganizationSelect component

--- studio/src/pages/organization/OrganizationSelect.tsx
+++ studio/src/pages/organization/OrganizationSelect.tsx
@@ -1,16 +1,17 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/Avatar';
 import { Button } from '@/components/Button';
+import { EmptyState } from '@/components/EmptyState';
+import { AuthUserDropdown } from '@/features/auth/AuthUserDropdown';
+import { ReleaseDropdown } from '@/features/cluster';
 import { OrganizationCreateButton } from '@/features/organization';
 import useAuthStore from '@/store/auth/authStore';
 import useOrganizationStore from '@/store/organization/organizationStore';
 import { Organization } from '@/types';
+import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useOutletContext } from 'react-router-dom';
+import BeatLoader from 'react-spinners/BeatLoader';
 import './organization.scss';
-import { EmptyState } from '@/components/EmptyState';
-import { useEffect } from 'react';
-import { AuthUserDropdown } from '@/features/auth/AuthUserDropdown';
-import { ReleaseDropdown } from '@/features/cluster';
 export default function OrganizationSelect() {
 	const { organizations, selectOrganization, getAllOrganizationByUser } = useOrganizationStore();
 	const { user } = useAuthStore();
@@ -26,9 +27,11 @@ export default function OrganizationSelect() {
 		navigate(`/organization/${org?._id}`);
 	}
 
-	useEffect(() => {
-		getAllOrganizationByUser();
-	}, []);
+	const { isFetching } = useQuery({
+		queryKey: ['organizations'],
+		queryFn: getAllOrganizationByUser,
+		refetchOnWindowFocus: false,
+	});
 
 	return (
 		<div className='p-6 relative'>
@@ -37,37 +40,45 @@ export default function OrganizationSelect() {
 				<AuthUserDropdown />
 			</div>
 			<div className='select-organization-container'>
-				{!!organizations.length && (
-					<h1 className='select-organization-title'>{t('organization.select')}</h1>
+				{isFetching ? (
+					<div className='flex justify-center items-center h-full'>
+						<BeatLoader color='#6884FD' size={24} margin={18} />
+					</div>
+				) : (
+					<>
+						{!!organizations.length && (
+							<h1 className='select-organization-title'>{t('organization.select')}</h1>
+						)}
+						<div className='select-organization-items'>
+							{user?.canCreateOrg && <OrganizationCreateButton onClick={openOrgCreateModal} />}
+							{organizations.length > 0 &&
+								organizations.map((organization) => (
+									<Button
+										variant='blank'
+										onClick={() => handleClickOrganization(organization)}
+										key={organization?._id}
+										className='select-organization-button'
+									>
+										<div className='select-organization-item'>
+											<Avatar size='4xl' square>
+												<AvatarImage src={organization.pictureUrl} alt={organization.name} />
+												<AvatarFallback name={organization?.name} color={organization?.color} />
+											</Avatar>
+											<div className='select-organization-info'>
+												<p className='select-organization-name'>{organization?.name}</p>
+												<p className='select-organization-role'>{organization?.role}</p>
+											</div>
+										</div>
+									</Button>
+								))}
+							{organizations.length === 0 && !user?.canCreateOrg && (
+								<EmptyState type='org' title={t('organization.empty_organization')}>
+									<p className='text-default'>{t('organization.empty_organization_desc')}</p>
+								</EmptyState>
+							)}
+						</div>
+					</>
 				)}
-				<div className='select-organization-items'>
-					{user?.canCreateOrg && <OrganizationCreateButton onClick={openOrgCreateModal} />}
-					{organizations.length > 0 &&
-						organizations.map((organization) => (
-							<Button
-								variant='blank'
-								onClick={() => handleClickOrganization(organization)}
-								key={organization?._id}
-								className='select-organization-button'
-							>
-								<div className='select-organization-item'>
-									<Avatar size='4xl' square>
-										<AvatarImage src={organization.pictureUrl} alt={organization.name} />
-										<AvatarFallback name={organization?.name} color={organization?.color} />
-									</Avatar>
-									<div className='select-organization-info'>
-										<p className='select-organization-name'>{organization?.name}</p>
-										<p className='select-organization-role'>{organization?.role}</p>
-									</div>
-								</div>
-							</Button>
-						))}
-					{organizations.length === 0 && !user?.canCreateOrg && (
-						<EmptyState type='org' title={t('organization.empty_organization')}>
-							<p className='text-default'>{t('organization.empty_organization_desc')}</p>
-						</EmptyState>
-					)}
-				</div>
 			</div>
 		</div>
 	);
