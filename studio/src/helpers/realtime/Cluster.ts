f8e1d57f3868048b66d2d7e266e787b7b4bab804
Enes Malik Ozer <enes@altogic.com>
2023-12-27 14:00:04 +0300

    Update Cluster class to handle an array of ClusterResourceStatus

--- studio/src/helpers/realtime/Cluster.ts
+++ studio/src/helpers/realtime/Cluster.ts
@@ -2,13 +2,14 @@ import useClusterStore from '@/store/cluster/clusterStore';
 import { ClusterResourceStatus, RealtimeActionParams } from '@/types';
 import { RealtimeActions } from './RealtimeActions';
 
-class Cluster implements RealtimeActions<ClusterResourceStatus> {
-	update(param: RealtimeActionParams<ClusterResourceStatus>): void {
+class Cluster implements RealtimeActions<ClusterResourceStatus[]> {
+	update(param: RealtimeActionParams<ClusterResourceStatus[]>): void {
 		useClusterStore.setState?.((prev) => ({
 			...prev,
 			clusterComponentsReleaseInfo: prev.clusterComponentsReleaseInfo.map((component) => ({
 				...component,
-				status: param.data.status,
+				status:
+					param.data.find((item) => item.name === `${component.module}-deployment`)?.status ?? '',
 			})),
 		}));
 	}
