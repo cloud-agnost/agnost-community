08678d6973bfa11f8a911652a6d6b2277fe962b4
Enes Malik Ozer <enes@altogic.com>
2023-12-28 13:34:23 +0300

    Refactor Feedback component to accept a className prop

--- studio/src/components/Alert/Feedback.tsx
+++ studio/src/components/Alert/Feedback.tsx
@@ -1,14 +1,15 @@
 import { Error, SuccessCheck } from '@/components/icons';
-
+import { cn } from '@/utils';
 interface Props {
-	success: boolean;
+	success?: boolean;
 	title: string;
 	description: string;
+	className?: string;
 }
-export default function Feedback({ success, title, description }: Props) {
+export default function Feedback({ success, title, description, className }: Props) {
 	const Icon = success ? SuccessCheck : Error;
 	return (
-		<div className='flex flex-col items-center p-8 space-y-4 text-center'>
+		<div className={cn('flex flex-col items-center p-8 space-y-4 text-center', className)}>
 			<Icon className='h-24 w-24' />
 			<h2 className='text-3xl font-semibold text-default'>{title}</h2>
 			<p className='text-subtle'>{description}</p>
