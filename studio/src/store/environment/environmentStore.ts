85e969e003e503b823a28fae3a6683762d65e70b
Enes Malik Ozer <enes@altogic.com>
2024-01-02 23:49:01 +0300

    Add persist middleware to environmentStore

--- studio/src/store/environment/environmentStore.ts
+++ studio/src/store/environment/environmentStore.ts
@@ -16,7 +16,7 @@ import {
 } from '@/types';
 import { DATE_TIME_FORMAT, formatDate, joinChannel } from '@/utils';
 import EnvironmentService from 'services/EnvironmentService.ts';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
 
 interface EnvironmentStore {
 	environments: Environment[];
@@ -57,130 +57,136 @@ const initialState: EnvironmentStore = {
 
 const useEnvironmentStore = create<EnvironmentStore & Actions>()(
 	devtools(
-		(set) => ({
-			...initialState,
-			getAppVersionEnvironment: async (params: getAppVersionEnvironmentParams) => {
-				const environment = await EnvironmentService.getAppVersionEnvironment(params);
-				set({ environment });
-				joinChannel(environment._id);
-				return environment;
-			},
-			getEnvironmentLogs: async (params: GetEnvironmentLogsParams) => {
-				const logs = await EnvironmentService.getEnvironmentLogs(params);
-				set({ envLogs: logs });
-			},
-			getEnvironmentLogsDetail: async (params: GetEnvironmentLogDetailsParams) => {
-				const log = await EnvironmentService.getEnvironmentLogsDetail(params);
-
-				const selectedLog = {
-					dbLogs: log.dbLogs?.map((log) => ({
-						timestamp: formatDate(log.startedAt, DATE_TIME_FORMAT),
-						message: log.message,
-						type: log.status,
-					})),
-					serverLogs: log.serverLogs?.map((log) => ({
-						timestamp: formatDate(log.startedAt, DATE_TIME_FORMAT),
-						message: log.message,
-						type: log.status,
-						pod: log.pod,
-					})),
-					schedulerLogs: log.schedulerLogs?.map((log) => ({
-						timestamp: formatDate(log.startedAt, DATE_TIME_FORMAT),
-						message: log.message,
-						type: log.status,
-					})),
-				} as SelectedEnvLog;
-
-				set((prev) => ({
-					selectedLog,
-					envLogs: prev.envLogs.map((envLog) => {
-						if (envLog._id === log._id) {
-							return log;
-						}
-						return envLog;
-					}),
-				}));
-			},
-			toggleAutoDeploy: async (params: ToggleAutoDeployParams) => {
-				try {
-					const environment = await EnvironmentService.toggleAutoDeploy(params);
+		persist(
+			(set) => ({
+				...initialState,
+				getAppVersionEnvironment: async (params: getAppVersionEnvironmentParams) => {
+					const environment = await EnvironmentService.getAppVersionEnvironment(params);
 					set({ environment });
+					joinChannel(environment._id);
 					return environment;
-				} catch (e) {
-					throw e as APIError;
-				}
-			},
-			suspendEnvironment: async (params: VersionParams) => {
-				try {
-					const environment = await EnvironmentService.suspendEnvironment(params);
-					set({ environment });
-				} catch (e) {
-					throw e as APIError;
-				}
-			},
-			activateEnvironment: async (params: VersionParams) => {
-				try {
-					const environment = await EnvironmentService.activateEnvironment(params);
-					set({ environment });
-				} catch (e) {
-					throw e as APIError;
-				}
-			},
-			redeployAppVersionToEnvironment: async (params: VersionParams) => {
-				try {
-					const environment = await EnvironmentService.redeployAppVersionToEnvironment(params);
-					set({ environment });
-				} catch (error) {
-					throw error as APIError;
-				}
-			},
-			restartApiServers: async (params: VersionParams) => {
-				try {
-					return await EnvironmentService.restartApiServers(params);
-				} catch (error) {
-					throw error as APIError;
-				}
-			},
-			updateEnvironmentTelemetryLogs: (params: UpdateEnvironmentTelemetryLogsParams) => {
-				return EnvironmentService.updateEnvironmentTelemetryLogs(params);
-			},
-			getEnvironmentResources: async (params: GetEnvironmentResourcesParams) => {
-				const resources = await EnvironmentService.getEnvironmentResources(params);
-				resources.forEach((resource) => {
-					joinChannel(resource._id);
-				});
-				set({ resources });
-				return resources;
-			},
-			openLogDetails: (log: EnvLog) => {
-				set({ log, isLogDetailsOpen: true });
-			},
-			closeLogDetails: () => {
-				set({ selectedLog: {} as SelectedEnvLog, isLogDetailsOpen: false });
-			},
-			updateApiServerConf: async (params: UpdateAPIServerConfParams) => {
-				try {
-					const apiServer = await EnvironmentService.updateAPIServerConf(params);
+				},
+				getEnvironmentLogs: async (params: GetEnvironmentLogsParams) => {
+					const logs = await EnvironmentService.getEnvironmentLogs(params);
+					set({ envLogs: logs });
+				},
+				getEnvironmentLogsDetail: async (params: GetEnvironmentLogDetailsParams) => {
+					const log = await EnvironmentService.getEnvironmentLogsDetail(params);
+
+					const selectedLog = {
+						dbLogs: log.dbLogs?.map((log) => ({
+							timestamp: formatDate(log.startedAt, DATE_TIME_FORMAT),
+							message: log.message,
+							type: log.status,
+						})),
+						serverLogs: log.serverLogs?.map((log) => ({
+							timestamp: formatDate(log.startedAt, DATE_TIME_FORMAT),
+							message: log.message,
+							type: log.status,
+							pod: log.pod,
+						})),
+						schedulerLogs: log.schedulerLogs?.map((log) => ({
+							timestamp: formatDate(log.startedAt, DATE_TIME_FORMAT),
+							message: log.message,
+							type: log.status,
+						})),
+					} as SelectedEnvLog;
+
 					set((prev) => ({
-						resources: prev.resources.map((resource) => {
-							if (resource._id === apiServer._id) {
-								return { ...resource, ...apiServer };
+						selectedLog,
+						envLogs: prev.envLogs.map((envLog) => {
+							if (envLog._id === log._id) {
+								return log;
 							}
-							return resource;
+							return envLog;
 						}),
 					}));
-					if (params.onSuccess) params.onSuccess();
-				} catch (e) {
-					if (params.onError) params.onError(e as APIError);
-					throw e;
-				}
-			},
-			reset: () => set(initialState),
-		}),
+				},
+				toggleAutoDeploy: async (params: ToggleAutoDeployParams) => {
+					try {
+						const environment = await EnvironmentService.toggleAutoDeploy(params);
+						set({ environment });
+						return environment;
+					} catch (e) {
+						throw e as APIError;
+					}
+				},
+				suspendEnvironment: async (params: VersionParams) => {
+					try {
+						const environment = await EnvironmentService.suspendEnvironment(params);
+						set({ environment });
+					} catch (e) {
+						throw e as APIError;
+					}
+				},
+				activateEnvironment: async (params: VersionParams) => {
+					try {
+						const environment = await EnvironmentService.activateEnvironment(params);
+						set({ environment });
+					} catch (e) {
+						throw e as APIError;
+					}
+				},
+				redeployAppVersionToEnvironment: async (params: VersionParams) => {
+					try {
+						const environment = await EnvironmentService.redeployAppVersionToEnvironment(params);
+						set({ environment });
+					} catch (error) {
+						throw error as APIError;
+					}
+				},
+				restartApiServers: async (params: VersionParams) => {
+					try {
+						return await EnvironmentService.restartApiServers(params);
+					} catch (error) {
+						throw error as APIError;
+					}
+				},
+				updateEnvironmentTelemetryLogs: (params: UpdateEnvironmentTelemetryLogsParams) => {
+					return EnvironmentService.updateEnvironmentTelemetryLogs(params);
+				},
+				getEnvironmentResources: async (params: GetEnvironmentResourcesParams) => {
+					const resources = await EnvironmentService.getEnvironmentResources(params);
+					resources.forEach((resource) => {
+						joinChannel(resource._id);
+					});
+					set({ resources });
+					return resources;
+				},
+				openLogDetails: (log: EnvLog) => {
+					set({ log, isLogDetailsOpen: true });
+				},
+				closeLogDetails: () => {
+					set({ selectedLog: {} as SelectedEnvLog, isLogDetailsOpen: false });
+				},
+				updateApiServerConf: async (params: UpdateAPIServerConfParams) => {
+					try {
+						const apiServer = await EnvironmentService.updateAPIServerConf(params);
+						set((prev) => ({
+							resources: prev.resources.map((resource) => {
+								if (resource._id === apiServer._id) {
+									return { ...resource, ...apiServer };
+								}
+								return resource;
+							}),
+						}));
+						if (params.onSuccess) params.onSuccess();
+					} catch (e) {
+						if (params.onError) params.onError(e as APIError);
+						throw e;
+					}
+				},
+				reset: () => set(initialState),
+			}),
 
-		{
-			name: 'environment',
-		},
+			{
+				name: 'environment-store',
+				partialize: (state) =>
+					Object.fromEntries(
+						Object.entries(state).filter(([key]) => ['environment'].includes(key)),
+					),
+			},
+		),
 	),
 );
 
