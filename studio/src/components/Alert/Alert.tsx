aa0780c0794fa669503ac364cd204ff8b7d7f8f3
Enes Malik Ozer <enes@altogic.com>
2024-01-04 21:20:37 +0300

    Reorder imports and fix import order in Alert component

--- studio/src/components/Alert/Alert.tsx
+++ studio/src/components/Alert/Alert.tsx
@@ -1,10 +1,9 @@
-import * as React from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
 
-import './alert.scss';
-import { SuccessCheck, Error } from '@/components/icons';
 import { CheckCircle, Warning, XCircle } from '@phosphor-icons/react';
+import './alert.scss';
 
 const alertVariants = cva('alert', {
 	variants: {
