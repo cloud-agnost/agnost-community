9b0b23dc2ffa15476cdd00df7a16921fa7155dab
Enes Malik Ozer <enes@altogic.com>
2024-01-04 15:50:48 +0300

    Add cron expression examples and separator component to TaskForm

--- studio/src/features/task/TaskForm.tsx
+++ studio/src/features/task/TaskForm.tsx
@@ -9,9 +9,10 @@ import {
 	FormMessage,
 } from '@/components/Form';
 import { Input } from '@/components/Input';
+import { Separator } from '@/components/Separator';
 import { Switch } from '@/components/Switch';
 import { CreateTaskSchema } from '@/types';
-import { translate as t } from '@/utils';
+import { describeCronExpression, translate as t } from '@/utils';
 import { useFormContext } from 'react-hook-form';
 import * as z from 'zod';
 
@@ -60,8 +61,62 @@ export default function TaskForm({ loading }: { loading?: boolean }) {
 								{...field}
 							/>
 						</FormControl>
-						<FormDescription>{t('task.syntax_description')}</FormDescription>
+						<div className='space-x-'>
+							<FormDescription>{t('task.syntax_description')}</FormDescription>
+
+							<div className='space-y-1 text-sm'>
+								<div className='flex items-center space-x-2 mt-4'>
+									<p className='text-default'>*</p>
+									<p className='text-subtle'>
+										any value (e.g. <span className='text-default'>*</span>)
+									</p>
+								</div>
+								<div className='flex items-center space-x-2'>
+									<p className='text-default'>,</p>
+									<p className='text-subtle'>
+										value list separator (e.g. <span className='text-default'>1,5</span>)
+									</p>
+								</div>
+								<div className='flex items-center space-x-2'>
+									<p className='text-default'>-</p>
+									<p className='text-subtle'>
+										range of values (e.g. <span className='text-default'>1-5</span>)
+									</p>
+								</div>
+								<div className='flex items-center space-x-2'>
+									<p className='text-default'>/</p>
+									<p className='text-subtle'>
+										step values (e.g. <span className='text-default'>*/2</span>)
+									</p>
+								</div>
+							</div>
+
+							<Separator className='my-4' />
+							<div className='space-y-1 text-sm'>
+								<div className='flex items-center space-x-2 mt-4'>
+									<p className='text-default'>0 0 * * 1-5</p>
+									<p className='text-subtle'>{describeCronExpression('0 0 * * 1-5')}</p>
+								</div>
+								<div className='flex items-center space-x-2'>
+									<p className='text-default'>1-59/2 * * * *</p>
+									<p className='text-subtle'>{describeCronExpression('1-59/2 * * * *')}</p>
+								</div>
+								<div className='flex items-center space-x-2'>
+									<p className='text-default'>0 0 * * *</p>
+									<p className='text-subtle'>{describeCronExpression('0 0 * * *')}</p>
+								</div>
+								<div className='flex items-center space-x-2'>
+									<p className='text-default'>*/30 * 1,5 * *</p>
+									<p className='text-subtle'>{describeCronExpression('*/30 1,5 * * *')}</p>
+								</div>
+								<div className='flex items-center space-x-2'>
+									<p className='text-default'>0 0 * * WED</p>
+									<p className='text-subtle'>{describeCronExpression('0 0 * * WED')}</p>
+								</div>
+							</div>
+						</div>
 						<FormMessage />
+						<Separator className='my-4' />
 					</FormItem>
 				)}
 			/>
