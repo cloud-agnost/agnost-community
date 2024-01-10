8ee5ccd1da552271f0ad34bc41bc945beed4ef33
Enes Malik Ozer <enes@altogic.com>
2024-01-10 16:37:08 +0300

    Fix onlyy cluster owner see cluster settings

--- studio/src/pages/profile/ProfileSettings.tsx
+++ studio/src/pages/profile/ProfileSettings.tsx
@@ -2,14 +2,23 @@ import { MENU_ITEMS_FOR_PROFILE_SETTINGS } from '@/constants';
 import { SettingsNavbar } from '@/features/version/SettingsNavbar';
 import { SettingsLayout } from '@/layouts/SettingsLayout';
 import { RequireAuth } from '@/router';
+import useAuthStore from '@/store/auth/authStore';
+import { useMemo } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
 
 export default function ProfileSettings() {
 	const { orgId } = useParams() as { orgId: string };
-	const settings = MENU_ITEMS_FOR_PROFILE_SETTINGS.map((setting) => ({
-		...setting,
-		href: setting.href.replace(':id', orgId as string),
-	}));
+	const user = useAuthStore((state) => state.user);
+	const settings = useMemo(() => {
+		return MENU_ITEMS_FOR_PROFILE_SETTINGS.filter(
+			(item) => !item.href.includes('cluster') || user?.isClusterOwner,
+		).map((item) => {
+			return {
+				...item,
+				href: item.href.replace(':orgId', orgId),
+			};
+		});
+	}, [user?.isClusterOwner]);
 
 	return (
 		<SettingsLayout
