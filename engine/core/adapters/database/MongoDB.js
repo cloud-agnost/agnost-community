b05f824637cb82f25f6d0d909bace55645cee698
Ümit Çakmak <umit.cakmak@altogic.com>
2024-01-13 16:48:26 +0300

    testing transactions

--- engine/core/adapters/database/MongoDB.js
+++ engine/core/adapters/database/MongoDB.js
@@ -447,6 +447,7 @@ export class MongoDB extends DatabaseBase {
 	 * Starts a new transaction on the database server. Any database CRUD operation that is executed after a call to `beginTransaction` will be executed within the transaction context. If the transaction is not committed then the changes will not be applied to the database.
 	 */
 	async beginTransaction(dbMeta) {
+		console.log("****beginTransaction");
 		if (this.transactionsSupported === null) {
 			// Check to see if transactions are supported by MongoDB deployment
 			const dbName = this.getAppliedDbName(dbMeta);
@@ -1132,6 +1133,7 @@ export class MongoDB extends DatabaseBase {
 		this.createSkipStage(options.skip, pipeline);
 		this.createLimitStage(options.limit, pipeline);
 
+		console.log("***session", this.session);
 		const dataCursor = await collection.aggregate(pipeline, {
 			allowDiskUse: true, // Lets the server know if it can use disk to store temporary results for the aggregation
 			raw: false, // Whether to return document results as raw BSON buffers
