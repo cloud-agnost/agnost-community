a173cfbf16aee8aa25cc270e6876ebb6bc7fabad
Enes Malik Ozer <enes@altogic.com>
2024-01-25 18:40:49 +0300

    Fix default values for search field and direction

--- studio/src/hooks/useSearch.tsx
+++ studio/src/hooks/useSearch.tsx
@@ -4,8 +4,8 @@ import { useSearchParams } from 'react-router-dom';
 
 export default function useSearch(data: any[]) {
 	const [searchParams] = useSearchParams();
-	const field = searchParams.get('f') ?? '';
-	const dir = (searchParams.get('d') ?? '') as 'asc' | 'desc';
+	const field = searchParams.get('f') ?? 'name';
+	const dir = (searchParams.get('d') ?? 'asc') as 'asc' | 'desc';
 	const filteredData = useMemo(() => {
 		if (searchParams.get('q')) {
 			const query = new RegExp(searchParams.get('q') as string, 'i');
