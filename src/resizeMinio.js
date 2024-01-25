d243e68bc2706ebccb23cf54f2c313db5eb9f1e2
Koray Oksay <koray.oksay@gmail.com>
2024-01-25 13:48:37 +0300

    Updates to resizeMinio to support standalone and distributed modes

--- src/resizeMinio.js
+++ src/resizeMinio.js
@@ -7,8 +7,50 @@ const router = express.Router();
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 const k8sCoreApi = kc.makeApiClient(k8s.CoreV1Api);
+const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
 
 const namespace = process.env.NAMESPACE;
+
+// Function to simulate sleep
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function scaleDeployment(deploymentName, replicas) {
+  // find the particular deployment
+  try {
+    res = await k8sApi.readNamespacedDeployment(deploymentName, namespace);
+    let deployment = res.body;
+
+    // edit
+    deployment.spec.replicas = replicas;
+
+    // replace
+    await k8sApi.replaceNamespacedDeployment(deploymentName, namespace, deployment);
+
+  } catch (error) {
+    throw new Error(JSON.stringify(error.body));
+  }
+}
+
+async function scaleStatefulSet(statefulSetName, replicas) {
+  // find the particular deployment
+  try {
+    res = await k8sApi.readNamespacedStatefulSet(statefulSetName, namespace);
+    let sts = res.body;
+
+    // edit
+    sts.spec.replicas = replicas;
+
+    // replace
+    await k8sApi.replaceNamespacedStatefulSet(statefulSetName, namespace, sts);
+
+  } catch (error) {
+    throw new Error(JSON.stringify(error.body));
+  }
+}
 
 async function resizeMinio(newSize) {
   const pvcPatch = {
@@ -23,10 +65,42 @@ async function resizeMinio(newSize) {
   const requestOptions = { headers: { 'Content-Type': 'application/merge-patch+json' }, };
 
   try {
-    for (i of ['0', '1', '2', '3']) {
-      const pvcName = 'export-minio-storage-' + i;
-      await k8sCoreApi.patchNamespacedPersistentVolumeClaim(pvcName, namespace, pvcPatch, undefined, undefined, undefined, undefined, undefined, requestOptions);
-      console.log('PersistentVolumeClaim ' + pvcName + ' was updated...');
+    const pvcList = await k8sCoreApi.listNamespacedPersistentVolumeClaim(namespace);
+    pvcList.body.items.forEach(async (pvc) => {
+      var pvcName = pvc.metadata.name;
+      if (pvcName.includes('minio')) {
+        await k8sCoreApi.patchNamespacedPersistentVolumeClaim(pvcName, namespace, pvcPatch, undefined, undefined, undefined, undefined, undefined, requestOptions);
+        console.log('PersistentVolumeClaim ' + pvcName + ' updated...');
+      }
+    });
+
+    await sleep(5000);
+
+    // MinIO might be a Deployment or a StatefulSet
+    isDeployment = false;
+    deploymentList = await k8sApi.listNamespacedDeployment(namespace);
+    deploymentList.body.items.forEach(async (depl) => {
+      var dname = depl.metadata.name;
+      if (dname.includes('minio')) {
+        isDeployment = true;
+        deploymentName = dname;
+      };
+    });
+
+    if (isDeployment) {
+      await scaleDeployment(deploymentName, 0);
+      await sleep(5000)
+      await scaleDeployment(deploymentName, 1);
+    } else {
+      stsList = await k8sApi.listNamespacedStatefulSet(namespace);
+      stsList.body.items.forEach(async (sts) => {
+        var stsName = sts.metadata.name;
+        if (stsName.includes('minio')) {
+          await scaleStatefulSet(stsName, 0);
+          await sleep(5000);
+          await scaleStatefulSet(stsName, 4);
+        };
+      });
     };
   } catch (error){
     console.error('Error updating minio PVC size: ', error.body);
