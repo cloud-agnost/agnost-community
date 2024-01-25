621eb2e0ce9cf722c2ad999796404c54630c4bdd
Enes Malik Ozer <enes@altogic.com>
2024-01-25 15:29:17 +0300

    Remove unnecessary CSS class from Loading component

--- studio/src/components/Loading/Loading.tsx
+++ studio/src/components/Loading/Loading.tsx
@@ -9,7 +9,7 @@ export default function Loading({ className, ...props }: LoadingProps) {
 	return (
 		<div
 			className={cn(
-				'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 animate-pulse',
+				'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10',
 				className,
 			)}
 		>
