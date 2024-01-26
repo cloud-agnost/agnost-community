dd493a5fac488c3036f7791d295d14a125ab6750
Enes Malik Ozer <enes@altogic.com>
2024-01-27 02:30:01 +0300

    Refactor VersionLogDetails component to use a resizable CodeEditor

--- studio/src/features/version/VersionLogs/VersionLogDetails.tsx
+++ studio/src/features/version/VersionLogs/VersionLogDetails.tsx
@@ -11,15 +11,15 @@ export default function VersionLogDetails({ open, onClose }: VersionLogDetailsPr
 	const { t } = useTranslation();
 	return (
 		<Drawer open={open} onOpenChange={onClose}>
-			<DrawerContent>
+			<DrawerContent className='h-full'>
 				<DrawerHeader>
 					<DrawerTitle>{t('version.log_details')}</DrawerTitle>
 				</DrawerHeader>
-				<div className='p-4 flex flex-col flex-1'>
+				<div className='p-4 flex flex-col flex-1 h-full resize-y overflow-auto'>
 					<CodeEditor
 						value={JSON.stringify(log, null, 2)}
 						defaultLanguage='json'
-						containerClassName='flex-1'
+						containerClassName='h-full'
 						className='h-full'
 						readonly
 						name={`versionLogDetails-${log._id}`}
