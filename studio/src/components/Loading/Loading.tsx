e9a4ace1e5dbe4dc57a21d5603c82aad100a2855
Enes Malik Ozer <enes@altogic.com>
2024-02-07 01:48:07 +0300

    Refactor Loading component to allow conditional rendering of loading animation

--- studio/src/components/Loading/Loading.tsx
+++ studio/src/components/Loading/Loading.tsx
@@ -1,19 +1,26 @@
 import { cn } from '@/utils';
-import BeatLoader from 'react-spinners/BeatLoader';
-import { LoaderSizeMarginProps } from 'react-spinners/helpers/props';
 
-interface LoadingProps extends LoaderSizeMarginProps {
+interface LoadingProps {
 	className?: string;
+	loading?: boolean;
 }
-export default function Loading({ className, ...props }: LoadingProps) {
+export default function Loading({ className, loading = true }: LoadingProps) {
 	return (
 		<div
 			className={cn(
 				'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10',
+				'flex space-x-6 justify-center items-center h-screen ',
 				className,
 			)}
 		>
-			<BeatLoader color='#0f87ff' size={16} margin={12} {...props} />
+			{loading && (
+				<>
+					<span className='sr-only'>Loading...</span>
+					<div className='size-5 bg-brand-primary rounded-full animate-bounce [animation-delay:-0.3s]' />
+					<div className='size-5 bg-brand-primary rounded-full animate-bounce [animation-delay:-0.15s]' />
+					<div className='size-5 bg-brand-primary rounded-full animate-bounce' />
+				</>
+			)}
 		</div>
 	);
 }
