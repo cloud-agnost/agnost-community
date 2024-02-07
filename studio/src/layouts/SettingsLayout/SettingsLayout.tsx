a618ae8de1799c2c09589d3e4965303764de402a
Enes Malik Ozer <enes@altogic.com>
2024-02-07 21:42:55 +0300

    Refactor SettingsLayout component to conditionally hide sidebar

--- studio/src/layouts/SettingsLayout/SettingsLayout.tsx
+++ studio/src/layouts/SettingsLayout/SettingsLayout.tsx
@@ -1,14 +1,16 @@
 import { ReactNode } from 'react';
 import { cn } from '@/utils';
+import useUtilsStore from '@/store/version/utilsStore';
 interface VersionLayoutProps {
 	children: ReactNode;
 	navbar: ReactNode;
 	className?: string;
 }
 export default function SettingsLayout({ children, navbar, className }: VersionLayoutProps) {
+	const { isSidebarOpen } = useUtilsStore();
 	return (
 		<div className={cn('flex gap-6 h-full', className)}>
-			<div className='h-full '>{navbar}</div>
+			<div className={cn(isSidebarOpen && 'hidden', 'h-full')}>{navbar}</div>
 			<div className='flex-1'>{children}</div>
 		</div>
 	);
