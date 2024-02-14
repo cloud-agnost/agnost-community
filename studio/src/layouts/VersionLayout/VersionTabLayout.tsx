23b36856ec11b48951ef69eea7bd0ebfc770dfec
Enes Malik Ozer <enes@altogic.com>
2024-02-14 16:48:44 +0300

    Fix button handler display in VersionTabLayout component

--- studio/src/layouts/VersionLayout/VersionTabLayout.tsx
+++ studio/src/layouts/VersionLayout/VersionTabLayout.tsx
@@ -88,7 +88,9 @@ export default function VersionTabLayout({
 							</span>
 						</Button>
 					) : (
-						handlerButton
+						// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+						// @ts-ignore
+						handlerButton?.props?.children[handlerButton?.props?.children.length - 1]
 					)}
 				</EmptyState>
 			);
@@ -101,6 +103,7 @@ export default function VersionTabLayout({
 			path: pathname + search,
 		});
 	}, [search]);
+
 	return (
 		<div className={cn('h-full space-y-4 p-4', className)}>
 			<div className={cn(!title ? 'flex items-center justify-between' : 'space-y-4')}>
