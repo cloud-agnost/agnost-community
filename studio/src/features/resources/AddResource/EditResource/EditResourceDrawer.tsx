7caef1ca04c675f1250dfbfc940663f9dd28064e
Enes Malik Ozer <enes@altogic.com>
2024-04-18 14:11:42 +0300

    Add restart functionality to managed resources

--- studio/src/features/resources/AddResource/EditResource/EditResourceDrawer.tsx
+++ studio/src/features/resources/AddResource/EditResource/EditResourceDrawer.tsx
@@ -1,17 +1,40 @@
+import { Button } from '@/components/Button';
+import { Description } from '@/components/Description';
 import { Drawer, DrawerContent, DrawerHeader, DrawerTitle } from '@/components/Drawer';
 import { Input } from '@/components/Input';
 import { Label } from '@/components/Label';
 import { UpdateAllowedRoles, UpdateResourceAccessConf } from '@/features/resources';
+import { useToast } from '@/hooks';
 import useResourceStore from '@/store/resources/resourceStore';
 import { ResourceCreateType } from '@/types';
+import { useMutation } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
 import EditSize from './EditSize';
 import EditVersionAndReplica from './EditVersionAndReplica';
 
 export default function EditResourceDrawer() {
 	const { t } = useTranslation();
 	const { isEditResourceModalOpen, closeEditResourceModal } = useResourceStore();
-	const { resourceToEdit, resourceConfig } = useResourceStore();
+	const { resourceToEdit, resourceConfig, restartManagedResource } = useResourceStore();
+	const { toast } = useToast();
+	const { orgId } = useParams() as Record<string, string>;
+	const { isPending, mutateAsync: restartResource } = useMutation({
+		mutationFn: () => restartManagedResource({ resourceId: resourceToEdit._id, orgId }),
+		mutationKey: ['restartManagedResource'],
+		onSuccess: () => {
+			toast({
+				title: t('resources.restart_success') as string,
+				action: 'success',
+			});
+		},
+		onError: (err) => {
+			toast({
+				title: err.details,
+				action: 'error',
+			});
+		},
+	});
 
 	return (
 		<Drawer open={isEditResourceModalOpen} onOpenChange={closeEditResourceModal}>
@@ -30,6 +53,18 @@ export default function EditResourceDrawer() {
 						<>
 							<EditSize />
 							<EditVersionAndReplica />
+						</>
+					)}
+
+					{resourceToEdit.managed && (
+						<>
+							<Description title={`Restart ${resourceToEdit.instance} server`}>
+								Please be informed that if you restart your resource you might observe interruption
+								in resource services until it completes its restart.
+							</Description>
+							<Button loading={isPending} onClick={restartResource}>
+								{t('general.restart')}
+							</Button>
 						</>
 					)}
 				</div>
